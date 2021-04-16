ee7da88a4ed9dc7d7a53f75112f82037a052cc61
Patrick <patway99@gmail.com>
2021-04-16 10:25:41 -0700

    gradle project config

---
+++ build.gradle.kts
@@ -0,0 +1,200 @@
+import com.github.jk1.license.LicenseReportExtension
+import nebula.plugin.contacts.Contact
+import nebula.plugin.contacts.ContactsExtension
+import nl.javadude.gradle.plugins.license.LicenseExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+
+    id("org.jetbrains.kotlin.jvm") version "1.4.31"
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
+    id("nebula.release") version "15.3.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+
+    id("com.github.hierynomus.license") version "0.15.0"
+    id("com.github.jk1.dependency-license-report") version "1.16"
+
+    id("nebula.maven-publish") version "17.3.2"
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.3.0"
+
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+    id("nebula.maven-apache-license") version "17.3.2"
+
+    id("nebula.integtest") version "7.0.9" apply false
+    id("org.openrewrite.rewrite") version "4.0.0"
+}
+
+apply(plugin = "nebula.integtest-standalone")
+apply(plugin = "nebula.publish-verification")
+
+configure<org.openrewrite.gradle.RewriteExtension> {
+    activeRecipe("org.openrewrite.java.format.AutoFormat")
+}
+
+configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
+    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
+}
+
+group = "org.openrewrite.recipe"
+description = "Eliminate legacy Spring patterns and migrate between major Spring Boot versions. Automatically."
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+    mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+signing {
+    setRequired({
+        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
+    })
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["nebula"])
+}
+
+configurations.all {
+    resolutionStrategy {
+        cacheChangingModulesFor(0, TimeUnit.SECONDS)
+        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
+    }
+}
+
+val rewriteVersion = "latest.integration"
+val testingFrameworksVersion = "latest.integration"
+dependencies {
+    compileOnly("org.projectlombok:lombok:latest.release")
+    annotationProcessor("org.projectlombok:lombok:latest.release")
+
+    implementation("org.openrewrite:rewrite-java:${rewriteVersion}")
+    implementation("org.openrewrite:rewrite-xml:${rewriteVersion}")
+    implementation("org.openrewrite:rewrite-properties:${rewriteVersion}")
+    implementation("org.openrewrite:rewrite-yaml:${rewriteVersion}")
+    implementation("org.openrewrite:rewrite-maven:${rewriteVersion}")
+
+    // eliminates "unknown enum constant DeprecationLevel.WARNING" warnings from the build log
+    // see https://github.com/gradle/kotlin-dsl-samples/issues/1301 for why (okhttp is leaking parts of kotlin stdlib)
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+
+    testImplementation("org.openrewrite:rewrite-test:${rewriteVersion}")
+
+    testImplementation("org.assertj:assertj-core:latest.release")
+    testImplementation("com.github.marschall:memoryfilesystem:latest.release")
+
+    // for generating properties migration configurations
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:latest.release")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:latest.release")
+    testImplementation("io.github.classgraph:classgraph:latest.release")
+
+    testRuntimeOnly("org.openrewrite:rewrite-java-11:${rewriteVersion}")
+    testRuntimeOnly("org.openrewrite:rewrite-java-8:${rewriteVersion}")
+
+    "integTestImplementation"("org.mapdb:mapdb:latest.release")
+    "integTestImplementation"("org.openrewrite:rewrite-java-11:${rewriteVersion}")
+    "integTestImplementation"("org.openrewrite:rewrite-java-8:${rewriteVersion}")
+
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+
+    options.isFork = true
+    options.forkOptions.executable = "javac"
+    options.compilerArgs.addAll(listOf("--release", "8"))
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
+
+tasks.withType(KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        useIR = true
+    }
+
+    doFirst {
+        destinationDir.mkdirs()
+    }
+}
+
+configure<ContactsExtension> {
+    val j = Contact("jkschneider@gmail.com")
+    j.moniker("Jonathan Schneider")
+
+    people["jkschneider@gmail.com"] = j
+}
+
+ configure<LicenseExtension> {
+     ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
+     skipExistingHeaders = true
+     header = project.rootProject.file("gradle/licenseHeader.txt")
+     mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
+     // exclude JavaTemplate shims from license check
+     exclude("src/main/resources/META-INF/rewrite/*.java")
+     strictCheck = true
+ }
+
+configure<LicenseReportExtension> {
+    renderers = arrayOf(com.github.jk1.license.render.CsvReportRenderer())
+}
+
+configure<PublishingExtension> {
+    publications {
+        named("nebula", MavenPublication::class.java) {
+            suppressPomMetadataWarningsFor("runtimeElements")
+
+            pom.withXml {
+                (asElement().getElementsByTagName("dependencies").item(0) as org.w3c.dom.Element).let { dependencies ->
+                    dependencies.getElementsByTagName("dependency").let { dependencyList ->
+                        var i = 0
+                        var length = dependencyList.length
+                        while (i < length) {
+                            (dependencyList.item(i) as org.w3c.dom.Element).let { dependency ->
+                                if ((dependency.getElementsByTagName("scope")
+                                        .item(0) as org.w3c.dom.Element).textContent == "provided") {
+                                    dependencies.removeChild(dependency)
+                                    i--
+                                    length--
+                                }
+                            }
+                            i++
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "rewrite-migrate-java"
+
+enableFeaturePreview("VERSION_ORDERING_V2")
