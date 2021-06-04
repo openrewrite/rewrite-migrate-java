b3480cec9ab72280bd81cfc10c1053df0ef90e0b
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-06-04 10:16:08 -0500

    migrate deprecated java.util.logging APIs (#21)

---
+++ src/main/java/org/openrewrite/java/migrate/logging/MigrateLoggerGlobalToGetGlobal.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.logging;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.Tree;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JContainer;
+import org.openrewrite.java.tree.JRightPadded;
+import org.openrewrite.java.tree.TypeUtils;
+import org.openrewrite.marker.Markers;
+
+import java.util.Collections;
+
+public class MigrateLoggerGlobalToGetGlobal extends Recipe {
+    @Override
+    public String getDisplayName() {
+        return "Migrate `Logger.global` to `Logger#getGlobal()`.";
+    }
+
+    @Override
+    public String getDescription() {
+        return "The preferred way to get the global logger object is via the call `Logger#getGlobal()`.";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new UsesType<>("java.util.logging.Logger");
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new MigrateLoggerGlobalToGetGlobalVisitor();
+    }
+
+    private static class MigrateLoggerGlobalToGetGlobalVisitor extends JavaVisitor<ExecutionContext> {
+        @Override
+        public J visitFieldAccess(J.FieldAccess fieldAccess, ExecutionContext ctx) {
+            J j = super.visitFieldAccess(fieldAccess, ctx);
+            J.FieldAccess asFieldAccess = (J.FieldAccess) j;
+
+            if (TypeUtils.isOfClassType(asFieldAccess.getTarget().getType(), "java.util.logging.Logger") && asFieldAccess.getSimpleName().equals("global")) {
+                j = new J.MethodInvocation(
+                        Tree.randomId(),
+                        asFieldAccess.getPrefix(),
+                        Markers.EMPTY,
+                        JRightPadded.build(asFieldAccess.getTarget()),
+                        null,
+                        asFieldAccess.getName().withName("getGlobal"),
+                        JContainer.build(Collections.emptyList()),
+                        TypeUtils.asMethod(asFieldAccess.getTarget().getType())
+                );
+            }
+
+            return j;
+        }
+
+
+    }
+
+}
---
+++ src/main/java/org/openrewrite/java/migrate/logging/MigrateLoggingMXBeanToGetPlatformMXBean.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.logging;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.J;
+
+public class MigrateLoggingMXBeanToGetPlatformMXBean extends Recipe {
+    private static final MethodMatcher LOG_MANAGER_GET_LOGGING_MX_BEAN_MATCHER = new MethodMatcher("java.util.logging.LogManager getLoggingMXBean()");
+
+    @Override
+    public String getDisplayName() {
+        return "Migrate `LogManager#getLoggingMXBean()` to `ManagementFactory#getPlatformMXBean(PlatformLoggingMXBean.class)`.";
+    }
+
+    @Override
+    public String getDescription() {
+        return "`java.util.logging.LoggingMXBean` is deprecated and replaced with `java.lang.management.PlatformLoggingMXBean`. Use `java.lang.management.ManagementFactory#getPlatformMXBean(PlatformLoggingMXBean.class)` instead.";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new UsesMethod<>(LOG_MANAGER_GET_LOGGING_MX_BEAN_MATCHER);
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new MigrateLoggingMXBeanToGetPlatformMXBeanVisitor();
+    }
+
+    private static class MigrateLoggingMXBeanToGetPlatformMXBeanVisitor extends JavaIsoVisitor<ExecutionContext> {
+        @Override
+        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+            J.MethodInvocation m = method;
+            if (LOG_MANAGER_GET_LOGGING_MX_BEAN_MATCHER.matches(m)) {
+                m = m.withTemplate(
+                        template("ManagementFactory.getPlatformMXBean(PlatformLoggingMXBean.class)")
+                                .imports("java.lang.management.ManagementFactory", "java.lang.management.PlatformLoggingMXBean")
+                                .build(),
+                        m.getCoordinates().replace()
+                );
+                maybeAddImport("java.lang.management.ManagementFactory");
+                maybeAddImport("java.lang.management.PlatformLoggingMXBean");
+                maybeRemoveImport("java.util.logging.LogManager");
+            }
+            return super.visitMethodInvocation(m, ctx);
+        }
+    }
+
+}
