42c53033bd9bdfdb90a48dbcb8f0763665d0fcd7
Jonathan Schneider <jkschneider@gmail.com>
2021-07-13 03:20:23 -0700

    License header

--- src/main/java/org/openrewrite/java/migrate/guava/NoGuavaMapsNewLinkedHashMap.java
+++ src/main/java/org/openrewrite/java/migrate/guava/NoGuavaMapsNewLinkedHashMap.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.migrate.guava;
 
 import org.openrewrite.ExecutionContext;
@@ -12,7 +27,7 @@ import org.openrewrite.java.tree.J;
 
 public class NoGuavaMapsNewLinkedHashMap extends Recipe {
     private static final MethodMatcher NEW_LINKED_HASH_MAP = new MethodMatcher("com.google.common.collect.Maps newLinkedHashMap()");
-    private static final MethodMatcher NEW_LINKED_HASH_MAP_ITERABLE = new MethodMatcher("com.google.common.collect.Maps newLinkedHashMap(java.util.Map)");
+    private static final MethodMatcher NEW_LINKED_HASH_MAP_WITH_MAP = new MethodMatcher("com.google.common.collect.Maps newLinkedHashMap(java.util.Map)");
     private static final MethodMatcher NEW_LINKED_HASH_MAP_CAPACITY = new MethodMatcher("com.google.common.collect.Maps newLinkedHashMapWithExpectedSize(int)");
 
     @Override
@@ -31,7 +46,7 @@ public class NoGuavaMapsNewLinkedHashMap extends Recipe {
             @Override
             public J.CompilationUnit visitCompilationUnit(J.CompilationUnit cu, ExecutionContext executionContext) {
                 doAfterVisit(new UsesMethod<>(NEW_LINKED_HASH_MAP));
-                doAfterVisit(new UsesMethod<>(NEW_LINKED_HASH_MAP_ITERABLE));
+                doAfterVisit(new UsesMethod<>(NEW_LINKED_HASH_MAP_WITH_MAP));
                 doAfterVisit(new UsesMethod<>(NEW_LINKED_HASH_MAP_CAPACITY));
                 return cu;
             }
@@ -45,7 +60,7 @@ public class NoGuavaMapsNewLinkedHashMap extends Recipe {
                     .imports("java.util.LinkedHashMap")
                     .build();
 
-            private final JavaTemplate newLinkedHashMapIterable = JavaTemplate.builder(this::getCursor, "new LinkedHashMap<>(#{any(java.util.Map)})")
+            private final JavaTemplate newLinkedHashMapWithMap = JavaTemplate.builder(this::getCursor, "new LinkedHashMap<>(#{any(java.util.Map)})")
                     .imports("java.util.LinkedHashMap")
                     .build();
 
@@ -59,10 +74,10 @@ public class NoGuavaMapsNewLinkedHashMap extends Recipe {
                     maybeRemoveImport("com.google.common.collect.Maps");
                     maybeAddImport("java.util.LinkedHashMap");
                     return method.withTemplate(newLinkedHashMap, method.getCoordinates().replace());
-                } else if (NEW_LINKED_HASH_MAP_ITERABLE.matches(method)) {
+                } else if (NEW_LINKED_HASH_MAP_WITH_MAP.matches(method)) {
                     maybeRemoveImport("com.google.common.collect.Maps");
                     maybeAddImport("java.util.LinkedHashMap");
-                    return method.withTemplate(newLinkedHashMapIterable, method.getCoordinates().replace(),
+                    return method.withTemplate(newLinkedHashMapWithMap, method.getCoordinates().replace(),
                             method.getArguments().get(0));
                 } else if (NEW_LINKED_HASH_MAP_CAPACITY.matches(method)) {
                     maybeRemoveImport("com.google.common.collect.Maps");
