430689d2aa7e2e94b4b69d2ea4d11fd462728456
traceyyoshima <tracey.yoshima@gmail.com>
2021-10-22 09:39:33 -0700

    Updated AddJDeprScanPlugin recipe.

---
+++ src/main/java/org/openrewrite/java/migrate/AddJDeprScanPlugin.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Incubating;
+import org.openrewrite.Recipe;
+import org.openrewrite.SourceFile;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.maven.AddPlugin;
+import org.openrewrite.maven.MavenVisitor;
+import org.openrewrite.maven.tree.Maven;
+
+import java.util.List;
+
+/**
+ * This imperative recipe will add the jdeprsacn plugin to a maven project. In the case of a multi-module project,
+ * this recipe will attempt to add the plugin to only the top level project.
+ */
+@Incubating(since = "0.2.0")
+public class AddJDeprScanPlugin extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Add JDeprScan Maven Plug-in";
+    }
+
+    @Override
+    public String getDescription() {
+        return "JDeprScan scans class files for uses of deprecated APIs.";
+    }
+
+    @Override
+    protected List<SourceFile> visit(List<SourceFile> before, ExecutionContext ctx) {
+        return ListUtils.map(before, s -> {
+            if (s instanceof Maven && "pom.xml".equals(s.getSourcePath().toString())) {
+                Maven maven = (Maven) s;
+                return (SourceFile) new AddJDeprScanPluginVisitor().visit(maven, ctx);
+            }
+            return s;
+        });
+    }
+
+    private static class AddJDeprScanPluginVisitor extends MavenVisitor {
+
+        @Override
+        public Maven visitMaven(Maven maven, ExecutionContext ctx) {
+            doAfterVisit(new AddPlugin(
+                    "org.apache.maven.plugins",
+                    "maven-jdeprscan-plugin",
+                    "3.0.0-alpha-1",
+                    "" +
+                            "<configuration>\n" +
+                            "   <release>11</release>\n" +
+                            "</configuration>",
+                    null,
+                    null));
+            return maven;
+        }
+    }
+}
