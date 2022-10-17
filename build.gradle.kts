e7a56236c5bf1a55db2176c50fd0401b6816fee3
Nate Danner <nate.danner@gmail.com>
2022-10-17 09:08:33 -0700

    chore: suppress jackson-databind (#133)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
 
 dependencyCheck {
     analyzers.assemblyEnabled = false
+    suppressionFile = "suppressions.xml"
     failBuildOnCVSS = 9.0F
 }
 
