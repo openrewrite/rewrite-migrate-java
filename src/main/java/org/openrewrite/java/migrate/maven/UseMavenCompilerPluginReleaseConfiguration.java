a17cf264aa499f2d33e7f446d19d7c213cfa3ef4
Nick McKinney <mckinneynicholas@gmail.com>
2022-12-07 12:32:26 -0500

    Java 11 and 17 recipes now handle explicit (non-property) maven-compiler-plugin source/target/release configuration (preferring release) (#149)
    * Java 11 and 17 recipes now handle explicit (non-property) maven-compiler-plugin source/target/release configuration (preferring release)
    
    * fixed project description, and pinned apache commons deps to specific major versions to avoid resolving to versions like 20030203.000550
    
    * license fix

---
+++ src/main/java/org/openrewrite/java/migrate/maven/UseMavenCompilerPluginReleaseConfiguration.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.maven;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.maven.MavenIsoVisitor;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.tree.Content;
+import org.openrewrite.xml.tree.Xml;
+
+import java.util.Optional;
+
+import static org.openrewrite.xml.AddOrUpdateChild.addOrUpdateChild;
+import static org.openrewrite.xml.FilterTagChildrenVisitor.filterChildren;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class UseMavenCompilerPluginReleaseConfiguration extends Recipe {
+    private static final XPathMatcher PLUGINS_MATCHER = new XPathMatcher("/project/build/plugins");
+
+    @Option(
+            displayName = "Release Version",
+            description = "The new value for the release configuration.",
+            example = "11"
+    )
+    String releaseVersion;
+
+    @Override
+    public String getDisplayName() {
+        return "Use Maven Compiler Plugin Release Configuration";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Replaces any explicit `source` or `target` configuration (if present) on the maven-compiler-plugin with `release`, and updates the `release` value if needed.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new MavenIsoVisitor<ExecutionContext>() {
+            @Override
+            public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext ctx) {
+                final Xml.Tag superVisit = super.visitTag(tag, ctx);
+                if (!PLUGINS_MATCHER.matches(getCursor())) {
+                    return superVisit;
+                }
+                Optional<Xml.Tag> compilerPluginConfig = superVisit.getChildren()
+                        .stream()
+                        .filter(plugin -> "plugin".equals(plugin.getName()) &&
+                                "org.apache.maven.plugins".equals(plugin.getChildValue("groupId").orElse(null)) &&
+                                "maven-compiler-plugin".equals(plugin.getChildValue("artifactId").orElse(null)))
+                        .findAny()
+                        .flatMap(it -> it.getChild("configuration"));
+                if (!compilerPluginConfig.isPresent()) {
+                    return superVisit;
+                }
+                Xml.Tag updated = filterChildren(superVisit, compilerPluginConfig.get(),
+                        child -> !(hasName(child, "source") || hasName(child, "target")));
+                if (updated == superVisit
+                        && !compilerPluginConfig.flatMap(it -> it.getChild("release")).isPresent()) {
+                    return superVisit;
+                }
+                updated = addOrUpdateChild(updated, compilerPluginConfig.get(),
+                        Xml.Tag.build("<release>" + releaseVersion + "</release>"), getCursor().getParentOrThrow());
+                return updated;
+            }
+
+            private boolean hasName(Content child, String name) {
+                return child instanceof Xml.Tag && name.equals(((Xml.Tag) child).getName());
+            }
+        };
+    }
+}
---
+++ src/test/java/org/openrewrite/java/migrate/maven/UseMavenCompilerPluginReleaseConfigurationTest.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.maven;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.maven.Assertions.pomXml;
+
+class UseMavenCompilerPluginReleaseConfigurationTest implements RewriteTest {
+    @Override
+    public void defaults(final RecipeSpec spec) {
+        spec.recipe(new UseMavenCompilerPluginReleaseConfiguration("11"));
+    }
+
+    @Test
+    void replacesSourceAndTargetConfig() {
+        rewriteRun(
+          pomXml("""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>org.sample</groupId>
+              <artifactId>sample</artifactId>
+              <version>1.0.0</version>
+              
+              <build>
+                <plugins>
+                  <plugin>
+                    <groupId>org.apache.maven.plugins</groupId>
+                    <artifactId>maven-compiler-plugin</artifactId>
+                    <version>3.8.0</version>
+                    <configuration>
+                      <source>1.8</source>
+                      <target>1.8</target>
+                    </configuration>
+                  </plugin>
+                </plugins>
+              </build>
+              
+            </project>
+            """,
+            """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>org.sample</groupId>
+              <artifactId>sample</artifactId>
+              <version>1.0.0</version>
+              
+              <build>
+                <plugins>
+                  <plugin>
+                    <groupId>org.apache.maven.plugins</groupId>
+                    <artifactId>maven-compiler-plugin</artifactId>
+                    <version>3.8.0</version>
+                    <configuration>
+                      <release>11</release>
+                    </configuration>
+                  </plugin>
+                </plugins>
+              </build>
+              
+            </project>
+            """
+          )
+        );
+    }
+
+    @Test
+    void upgradesExistingReleaseConfig() {
+        rewriteRun(
+          pomXml("""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>org.sample</groupId>
+              <artifactId>sample</artifactId>
+              <version>1.0.0</version>
+              
+              <build>
+                <plugins>
+                  <plugin>
+                    <groupId>org.apache.maven.plugins</groupId>
+                    <artifactId>maven-compiler-plugin</artifactId>
+                    <version>3.8.0</version>
+                    <configuration>
+                      <release>10</release>
+                    </configuration>
+                  </plugin>
+                </plugins>
+              </build>
+              
+            </project>
+            """,
+            """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>org.sample</groupId>
+              <artifactId>sample</artifactId>
+              <version>1.0.0</version>
+              
+              <build>
+                <plugins>
+                  <plugin>
+                    <groupId>org.apache.maven.plugins</groupId>
+                    <artifactId>maven-compiler-plugin</artifactId>
+                    <version>3.8.0</version>
+                    <configuration>
+                      <release>11</release>
+                    </configuration>
+                  </plugin>
+                </plugins>
+              </build>
+              
+            </project>
+            """
+          )
+        );
+    }
+
+    @Test
+    void doesNotChangeIfNoSourceOrTargetConfig() {
+        rewriteRun(
+          pomXml("""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>org.sample</groupId>
+              <artifactId>sample</artifactId>
+              <version>1.0.0</version>
+              
+              <build>
+                <plugins>
+                  <plugin>
+                    <groupId>org.apache.maven.plugins</groupId>
+                    <artifactId>maven-compiler-plugin</artifactId>
+                    <version>3.8.0</version>
+                    <configuration>
+                      <compilerArgs>-parameters</compilerArgs>
+                    </configuration>
+                  </plugin>
+                </plugins>
+              </build>
+              
+            </project>
+            """
+          )
+        );
+    }
+}
