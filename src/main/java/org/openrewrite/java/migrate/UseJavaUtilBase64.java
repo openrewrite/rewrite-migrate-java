4924d328159a63bbf6e41a45681e234e977cdb21
Kun Li <kun@moderne.io>
2023-03-06 00:13:23 -0800

    fix: fix ClassNotFoundException error to pass ci build

--- src/main/java/org/openrewrite/java/migrate/UseJavaUtilBase64.java
+++ src/main/java/org/openrewrite/java/migrate/UseJavaUtilBase64.java
@@ -23,7 +23,6 @@ import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaSourceFile;
 
-import java.util.Base64;
 
 public class UseJavaUtilBase64 extends Recipe {
     private final String sunPackage;
@@ -117,11 +116,13 @@ public class UseJavaUtilBase64 extends Recipe {
                 J.NewClass c = (J.NewClass) super.visitNewClass(newClass, ctx);
                 if (newBase64Encoder.matches(c)) {
                     return c.withTemplate(
-                            JavaTemplate.compile(this, "getEncoder",
-                            Base64::getEncoder).build(),
+                            JavaTemplate.builder(this::getCursor, "Base64.getEncoder()")
+                                .imports("java.util.Base64")
+                                .build(),
                             c.getCoordinates().replace()
                     );
                 }
+
                 if (newBase64Decoder.matches(newClass)) {
                     return c.withTemplate(getDecoderTemplate, c.getCoordinates().replace());
                 }
