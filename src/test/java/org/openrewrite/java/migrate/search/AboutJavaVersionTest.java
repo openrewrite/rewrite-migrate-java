861185e44dacc936463f02230348915d1bd4a0ba
Tim te Beek <timtebeek@gmail.com>
2023-03-25 21:15:25 +0000

    refactor: JUnit Jupiter best practices
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/java/migrate/search/AboutJavaVersionTest.java
+++ src/test/java/org/openrewrite/java/migrate/search/AboutJavaVersionTest.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class AboutJavaVersionTest implements RewriteTest {
+class AboutJavaVersionTest implements RewriteTest {
 
     @Test
     void aboutJavaVersion() {
