50e7478d1bcfecd0be0ab5a6eb8e4c0ea55dc5b7
Tim te Beek <tim@moderne.io>
2023-03-31 10:17:03 +0200

    Replace Guava Optional with JDK Optional (#202)
    * Prefer java.util.Optional over Guava Optional
    
    * Implement and document a few more cases
    
    * Implement and document a few more cases
    
    * Implement PreferJavaUtilOptionalOrElseNull
    
    * Adopt `firstChoice.or(() -> secondChoice)`
    
    Co-authored-by: Aurélien Mino <155828+murdos@users.noreply.github.com>
    
    * Address review comments
    
    * Add applicability test on a minimum of Java 9
    
    * Drop UsesJavaVersion as it's a visitor, not a recipe
    
    * Add NoGuavaOptionalFromJavaUtil
    
    * Add NoGuavaOptionalToJavaUtil
    
    * Use better visitor names
    
    * Add PreferJavaUtilOptionalOrSupplier
    
    * Only use or(Supplier<Optional>) on Java 9+
    
    * Convert Optional before Function, to match transform(Function)
    
    * Cover orSupplierToOrElseGetWithSupplierArgument
    
    ---------
    
    Co-authored-by: Aurélien Mino <155828+murdos@users.noreply.github.com>

---
+++ src/main/java/org/openrewrite/java/migrate/guava/PreferJavaUtilOptionalOrSupplier.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.guava;
+
+import org.openrewrite.Applicability;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesJavaVersion;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PreferJavaUtilOptionalOrSupplier extends Recipe {
+
+    static final MethodMatcher METHOD_MATCHER = new MethodMatcher("com.google.common.base.Optional or(com.google.common.base.Optional)");
+
+    @Override
+    public String getDisplayName() {
+        return "Prefer `java.util.Optional#or(Supplier<T extends java.util.Optional<T>>)`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Prefer `java.util.Optional#or(Supplier<T extends java.util.Optional<T>>)` over `com.google.common.base.Optional#or(com.google.common.base.Optional).";
+    }
+
+    @Override
+    public Duration getEstimatedEffortPerOccurrence() {
+        return Duration.ofMinutes(5);
+    }
+
+    @Override
+    public Set<String> getTags() {
+        return new HashSet<>(Arrays.asList("RSPEC-4738", "guava"));
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getApplicableTest() {
+        return Applicability.and(
+                new UsesJavaVersion<>(9),
+                new UsesType<>("com.google.common.base.Optional"));
+    }
+
+    @Override
+    protected UsesMethod<ExecutionContext> getSingleSourceApplicableTest() {
+        return new UsesMethod<>(METHOD_MATCHER);
+    }
+
+    @Override
+    protected JavaIsoVisitor<ExecutionContext> getVisitor() {
+        return new PreferJavaUtilOptionalOrSupplierVisitor();
+    }
+
+    private static class PreferJavaUtilOptionalOrSupplierVisitor extends JavaIsoVisitor<ExecutionContext> {
+        @Override
+        public J.CompilationUnit visitCompilationUnit(J.CompilationUnit cu, ExecutionContext executionContext) {
+            J.CompilationUnit c = super.visitCompilationUnit(cu, executionContext);
+            maybeAddImport("java.util.Optional");
+            maybeRemoveImport("com.google.common.base.Optional");
+            return c;
+        }
+
+        @Override
+        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
+            J.MethodInvocation j = super.visitMethodInvocation(method, executionContext);
+            if (METHOD_MATCHER.matches(method)) {
+                j = j.withTemplate(
+                        JavaTemplate.builder(this::getCursor, "#{any(java.util.Optional)}.or(() -> #{any(java.util.Optional)})")
+                                .imports("java.util.Optional")
+                                .build(),
+                        method.getCoordinates().replace(),
+                        j.getSelect(),
+                        j.getArguments().get(0)
+                );
+            }
+            return j;
+        }
+    }
+}
---
+++ src/test/java/org/openrewrite/java/migrate/guava/PreferJavaUtilOptionalTest.java
@@ -0,0 +1,335 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.guava;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ExpectedToFail;
+import org.openrewrite.Issue;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.java.Assertions.javaVersion;
+
+@Issue("https://github.com/openrewrite/rewrite-migrate-java/issues/197")
+class PreferJavaUtilOptionalTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(
+            Environment.builder()
+              .scanRuntimeClasspath("org.openrewrite.java.migrate.guava")
+              .build()
+              .activateRecipes("org.openrewrite.java.migrate.guava.NoGuava")
+          )
+          .parser(JavaParser.fromJavaVersion().classpath("rewrite-java", "guava"));
+    }
+
+    @Test
+    void absentToEmpty() {
+        //language=java
+        rewriteRun(java("""
+          import com.google.common.base.Optional;
+
+          class A {
+              Optional<String> foo() {
+                  return Optional.absent();
+              }
+          }
+          """, """
+          import java.util.Optional;
+
+          class A {
+              Optional<String> foo() {
+                  return Optional.empty();
+              }
+          }
+          """));
+    }
+
+    @Test
+    void orNullToOrElseNull() {
+        // Comparison to java.util.Optional: this method is equivalent to Java 8's Optional.orElse(null).
+        //language=java
+        rewriteRun(java("""
+              import com.google.common.base.Optional;
+
+              class A {
+                  String foo(Optional<String> optional) {
+                      return optional.orNull();
+                  }
+              }
+              """, """
+              import java.util.Optional;
+
+              class A {
+                  String foo(Optional<String> optional) {
+                      return optional.orElse(null);
+                  }
+              }
+              """));
+    }
+
+    @Test
+    void orToOrElse() {
+        //language=java
+        rewriteRun(java("""
+          import com.google.common.base.Optional;
+
+          class A {
+              Optional<String> foo(Optional<String> optional) {
+                  return optional.or("other");
+              }
+          }
+          """, """
+          import java.util.Optional;
+
+          class A {
+              Optional<String> foo(Optional<String> optional) {
+                  return optional.orElse("other");
+              }
+          }
+          """));
+    }
+
+    @Test
+    void orSupplierToOrElseGetWithLambda() {
+        //language=java
+        rewriteRun(java("""
+          import com.google.common.base.Optional;
+
+          class A {
+              Optional<String> foo(Optional<String> optional) {
+                  return optional.or(() -> "other");
+              }
+          }
+          """, """
+          import java.util.Optional;
+
+          class A {
+              Optional<String> foo(Optional<String> optional) {
+                  return optional.orElseGet(() -> "other");
+              }
+          }
+          """));
+    }
+
+    @Test
+    void orSupplierToOrElseGetWithSupplierArgument() {
+        //language=java
+        rewriteRun(
+          java("""
+              import com.google.common.base.Optional;
+              import com.google.common.base.Supplier;
+
+              class A {
+                  String foo(Optional<String> optional, Supplier<String> supplier) {
+                      return optional.or(supplier);
+                  }
+              }
+              """, """
+              import java.util.Optional;
+              import java.util.function.Supplier;
+
+              class A {
+                  String foo(Optional<String> optional, Supplier<String> supplier) {
+                      return optional.orElseGet(supplier);
+                  }
+              }
+              """));
+    }
+
+    @Test
+    void transformToMap() {
+        //language=java
+        rewriteRun(java("""
+          import com.google.common.base.Optional;
+
+          class A {
+              Optional<String> foo(Optional<String> optional) {
+                  return optional.transform(String::toUpperCase);
+              }
+          }
+          """, """
+          import java.util.Optional;
+
+          class A {
+              Optional<String> foo(Optional<String> optional) {
+                  return optional.map(String::toUpperCase);
+              }
+          }
+          """));
+    }
+
+    @Test
+    void removeFromJavaUtil() {
+        //language=java
+        rewriteRun(java("""
+              import com.google.common.base.Optional;
+
+              class A {
+                  Optional<String> foo(java.util.Optional<String> optional) {
+                      return Optional.fromJavaUtil(optional);
+                  }
+              }
+              """, """
+              import java.util.Optional;
+
+              class A {
+                  Optional<String> foo(java.util.Optional<String> optional) {
+                      return optional;
+                  }
+              }
+              """));
+    }
+
+    @Test
+    void removeToJavaUtil() {
+        //language=java
+        rewriteRun(java("""
+              import com.google.common.base.Optional;
+
+              class A {
+                  boolean foo() {
+                      return Optional.absent().toJavaUtil().isEmpty();
+                  }
+              }
+              """, """
+              import java.util.Optional;
+
+              class A {
+                  boolean foo() {
+                      return Optional.empty().isEmpty();
+                  }
+              }
+              """));
+    }
+
+    @Test
+    void orOptionalToOrSupplier() {
+        // Comparison to java.util.Optional: this method has no equivalent in Java 8's Optional class; write thisOptional.isPresent() ? thisOptional : secondChoice instead.
+        //language=java
+        rewriteRun(
+          spec -> spec.allSources(s -> s.markers(javaVersion(9))),
+          java("""
+              import com.google.common.base.Optional;
+
+              class A {
+                  Optional<String> foo(Optional<String> firstChoice, Optional<String> secondChoice) {
+                      return firstChoice.or(secondChoice);
+                  }
+              }
+              """, """
+              import java.util.Optional;
+
+              class A {
+                  Optional<String> foo(Optional<String> firstChoice, Optional<String> secondChoice) {
+                      return firstChoice.or(() -> secondChoice);
+                  }
+              }
+              """));
+    }
+
+    @Nested
+    class NotYetImplemented {
+
+        @Test
+        @ExpectedToFail("Not yet implemented")
+        void getCatchIllegalStateExceptionToNoSuchElementException() {
+            // > when the value is absent, this method throws IllegalStateException, whereas the Java 8 counterpart throws NoSuchElementException.
+            // Sure hope no one actually does this, but you never know.
+            //language=java
+            rewriteRun(java("""
+              import com.google.common.base.Optional;
+
+              class A {
+                  String foo(Optional<String> optional) {
+                      try {
+                          return optional.get();
+                      } catch (IllegalStateException e) {
+                          return "";
+                      }
+                  }
+              }
+              """, """
+              import java.util.Optional;
+
+              class A {
+                  String foo(Optional<String> optional) {
+                      try {
+                          return optional.get();
+                      } catch (NoSuchElementException e) {
+                          return "";
+                      }
+                  }
+              }
+              """));
+        }
+
+        @Test
+        @ExpectedToFail("Not yet implemented")
+        void asSetToStreamCollectToSet() {
+            // Comparison to java.util.Optional: this method has no equivalent in Java 8's Optional class. However, some use cases can be written with calls to optional.stream().
+            //language=java
+            rewriteRun(java("""
+              import com.google.common.base.Optional;
+
+              class A {
+                  Set<String> foo(Optional<String> optional) {
+                      return optional.asSet();
+                  }
+              }
+              """, """
+              import java.util.Optional;
+              import java.util.Set;
+              import java.util.stream.Collectors;
+
+              class A {
+                  Set<String> foo(Optional<String> optional) {
+                      return optional.stream().collect(Collectors.toSet());
+                  }
+              }
+              """));
+        }
+
+        @Test
+        @ExpectedToFail("Not yet implemented")
+        void presentInstances() {
+            // Comparison to java.util.Optional: this method has no equivalent in Java 8's Optional class; use optionals.stream().filter(Optional::isPresent).map(Optional::get) instead.
+            //language=java
+            rewriteRun(java("""
+              import com.google.common.base.Optional;
+
+              class A {
+                  Iterable<String> foo(Iterable<Optional<String>> optionals) {
+                      return Optional.presentInstances(optionals);
+                  }
+              }
+              """, """
+              import java.util.Optional;
+              import java.util.stream.Collectors;
+                        
+              class A {
+                  Iterable<String> foo(Iterable<Optional<String>> optionals) {
+                      return optionals.stream().flatMap(Optional::stream).collect(Collectors.toList());
+                  }
+              }
+              """));
+        }
+    }
+}
