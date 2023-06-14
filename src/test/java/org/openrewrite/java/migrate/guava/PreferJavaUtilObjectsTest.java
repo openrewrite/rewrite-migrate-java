bbf01db730553f64479ded719ab9912a8727fbc0
Tim te Beek <tim@moderne.io>
2023-06-14 10:30:01 +0200

    Add DocumentExample and apply review suggestions

---
+++ src/test/java/org/openrewrite/java/migrate/guava/PreferJavaUtilObjectsTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.guava;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class PreferJavaUtilObjectsTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(
+            Environment.builder()
+              .scanRuntimeClasspath("org.openrewrite.java.migrate.guava")
+              .build()
+              .activateRecipes("org.openrewrite.java.migrate.guava.NoGuava")
+          )
+          .parser(JavaParser.fromJavaVersion().classpath("rewrite-java", "guava"));
+    }
+
+    @DocumentExample
+    @Test
+    void preconditionsCheckNotNullToObjectsRequireNonNull() {
+        //language=java
+        rewriteRun(java("""
+          import com.google.common.base.Preconditions;
+
+          class A {
+              Object foo(Object obj) {
+                  return Preconditions.checkNotNull(obj);
+              }
+          }
+          """, """
+          import java.util.Objects;
+
+          class A {
+              Object foo(Object obj) {
+                  return Objects.requireNonNull(obj);
+              }
+          }
+          """));
+    }
+
+    @Test
+    void preconditionsCheckNotNullToObjectsRequireNonNullStatic() {
+        //language=java
+        rewriteRun(java("""
+          import static com.google.common.base.Preconditions.checkNotNull;
+
+          class A {
+              Object foo(Object obj) {
+                  return checkNotNull(obj);
+              }
+          }
+          """, """
+          import static java.util.Objects.requireNonNull;
+
+          class A {
+              Object foo(Object obj) {
+                  return requireNonNull(obj);
+              }
+          }
+          """));
+    }
+
+}
