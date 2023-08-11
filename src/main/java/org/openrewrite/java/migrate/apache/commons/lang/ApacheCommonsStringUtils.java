be8922301a63e2aec95217ac4d5afebbb0bcfd83
AlekSimpson <alek@asu.me>
2023-08-11 14:57:17 -0700

    more recipes, all moved into one class

---
+++ src/main/java/org/openrewrite/java/migrate/apache/commons/lang/ApacheCommonsStringUtils.java
@@ -0,0 +1,344 @@
+package org.openrewrite.java.migrate.apache.commons.lang;
+
+import com.google.errorprone.refaster.annotation.AfterTemplate;
+import com.google.errorprone.refaster.annotation.BeforeTemplate;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class ApacheCommonsStringUtils {
+    public static class Chop {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.chop(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.substring(0, s.length() - 1);
+        }
+    }
+
+    public static class DefaultString {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.defaultString(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return Objects.toString(s);
+        }
+    }
+
+    public static class Equals {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.equals(s, "string");
+        }
+
+        @AfterTemplate
+        boolean after(String s) {
+            return s.equals("string");
+        }
+    }
+
+    public static class IsEmpty {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.isEmpty(s);
+        }
+
+        @AfterTemplate
+        boolean after(String s) {
+            return s.isEmpty();
+        }
+    }
+
+    public static class Replace {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.replace(s, "o", "z");
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.replaceAll("o", "z");
+        }
+    }
+
+    public static class Split {
+        @BeforeTemplate
+        String[] before(String s) {
+            return StringUtils.split(s);
+        }
+
+        @AfterTemplate
+        String[] after(String s) {
+            return s.split(" ");
+        }
+    }
+
+    public static class StripEnd {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.stripEnd(s, "suffix");
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.endsWith("suffix") ? s.substring(0, s.lastIndexOf("suffix")) : s;
+        }
+    }
+
+    // -------------
+
+    public static class Strip {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.strip(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.trim();
+        }
+    }
+
+    //public static class LeftPad {
+    //    @BeforeTemplate
+    //    String before(String s) {
+    //        return StringUtils.leftPad(s, 5);
+    //    }
+
+    //    @AfterTemplate
+    //    String after(String s) {
+    //        return String.format("%" + 5 + "s", s);
+    //    }
+    //}
+
+    //public static class RightPad {
+    //    @BeforeTemplate
+    //    String before(String s) {
+    //        return StringUtils.rightPad(s, 5);
+    //    }
+
+    //    @AfterTemplate
+    //    String after(String s) {
+    //        return String.format("%" + (-5) + "s", s);
+    //    }
+    //}
+
+    public static class Join {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.join(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return String.join(s);
+        }
+    }
+
+    public static class DeleteWhitespace {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.deleteWhitespace(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.replaceAll("\\s+", "");
+        }
+    }
+
+    public static class EndsWithIgnoreCase {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.endsWithIgnoreCase(s, "suffix");
+        }
+
+        @AfterTemplate
+        boolean after(String s) {
+            return s.toUpperCase().endsWith("suffix".toUpperCase());
+        }
+    }
+
+    public static class IsAlphanumeric {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.isAlphanumeric(s);
+        }
+
+        @AfterTemplate
+        boolean after(String s) {
+            return s.matches("^[a-zA-Z0-9]*$");
+        }
+    }
+
+    public static class IsAlphanumericSpace {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.isAlphanumericSpace("^[a-zA-Z0-9\\s]*$");
+        }
+    }
+
+    public static class Left {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.left(s, 5);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.substring(0, 5);
+        }
+    }
+
+    public static class Lowercase {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.lowerCase(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.toLowerCase();
+        }
+    }
+
+    public static class Uppercase {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.upperCase(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.toUpperCase();
+        }
+    }
+
+    public static class ReplaceOnce {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.replaceOnce(s, "search", "replacement");
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.replaceFirst(Pattern.quote("search"), "replacement");
+        }
+    }
+
+    public static class Reverse {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.reverse(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return new StringBuilder(s).reverse().toString();
+        }
+    }
+
+    public static class Contains {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.contains(s, "search");
+        }
+
+        @AfterTemplate
+        boolean after(String s) {
+            return s.contains("search");
+        }
+    }
+
+    public static class Substring {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.substring(s, 1, 4);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.substring(1, 4);
+        }
+    }
+
+    public static class StartsWith {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.startsWith(s, "prefix");
+        }
+
+        @AfterTemplate
+        boolean after(String s) {
+            return s.startsWith("prefix");
+        }
+    }
+
+    public static class Trim {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.trim(s);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.trim();
+        }
+    }
+
+    public static class EqualsIgnoreCase {
+        @BeforeTemplate
+        boolean before(String s) {
+            return StringUtils.equalsIgnoreCase(s, "other");
+        }
+
+        @AfterTemplate
+        boolean after(String s) {
+            return s.equalsIgnoreCase("other");
+        }
+    }
+
+    public static class ReplaceTest {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.replace(s, "target", "replacement");
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.replaceAll("target", "replacement");
+        }
+    }
+
+    public static class Repeat {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.repeat(s, 5);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return new String(new char[5]).replace("\0", s);
+        }
+    }
+
+    public static class Overlay {
+        @BeforeTemplate
+        String before(String s) {
+            return StringUtils.overlay(s, "overlay", 2, 5);
+        }
+
+        @AfterTemplate
+        String after(String s) {
+            return s.substring(0, 2) + "overlay" + s.substring(5);
+        }
+    }
+
+}
--- src/test/java/org/openrewrite/java/migrate/apache/commons/lang/ApacheCommonsStringUtilsTest.java
+++ src/test/java/org/openrewrite/java/migrate/apache/commons/lang/ApacheCommonsStringUtilsTest.java
@@ -23,6 +23,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
+@SuppressWarnings({"UnnecessaryCallToStringValueOf", "ConstantValue"})
 class ApacheCommonsStringUtilsTest implements RewriteTest {
 
     @Override
@@ -34,195 +35,271 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @DocumentExample
     void defaultString() {
         rewriteRun(
-          spec -> spec.recipe(new DefaultStringRecipe()),
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.DefaultStringRecipe()),
           //language=java
-          java("""
-            import org.apache.commons.lang3.StringUtils;
-                          
-            class Foo {
-                String in = "foo";
-                String out = StringUtils.defaultString(in);
-            }
-            """, """
-            import java.util.Objects;
-                          
-            class Foo {
-                String in = "foo";
-                String out = Objects.toString(in);
-            }
-            """)
+          java(
+            """
+              import org.apache.commons.lang3.StringUtils;
+              
+              class Foo {
+                  String in = "foo";
+                  String out = StringUtils.defaultString(in);
+              }
+              """,
+            """
+              import java.util.Objects;
+              
+              class Foo {
+                  String in = "foo";
+                  String out = Objects.toString(in);
+              }
+              """
+          )
         );
     }
 
     @Test
     void defaultStringStatic() {
         rewriteRun(
-          spec -> spec.recipe(new DefaultStringRecipe()),
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.DefaultStringRecipe()),
           //language=java
-          java("""
+          java(
+          """
             import static org.apache.commons.lang3.StringUtils.defaultString;
                           
             class Foo {
                 String in = "foo";
                 String out = defaultString(in);
             }
-            """, """
+            """,
+          """
             import java.util.Objects;
                           
             class Foo {
                 String in = "foo";
                 String out = Objects.toString(in);
             }
-            """)
+            """
+          )
         );
     }
 
     @Test
     void isEmpty() {
         rewriteRun(
-          spec -> spec.recipe(new IsEmptyRecipe()),
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.IsEmptyRecipe()),
           //language=java
-          java("""
+          java(
+          """
             import org.apache.commons.lang3.StringUtils;
     
             class Foo {
                 String in = "foo";
                 boolean out = StringUtils.isEmpty(in);
             }
-            """, """
+            """,
+          """
             class Foo {
                 String in = "foo";
                 boolean out = in.isEmpty();
             }
-            """)
+            """
+          )
         );
     }
 
     @Test
     void splitTest() {
         rewriteRun(
-          spec -> spec.recipe(new SplitRecipe()),
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.SplitRecipe()),
           //language=java
-          java("""
+          java(
+          """
             import org.apache.commons.lang3.StringUtils;
             
             class Foo {
                 String in = "foo";
                 String[] out = StringUtils.split(in);
             }
-            """, """
+            """,
+          """
             class Foo {
                 String in = "foo";
                 String[] out = in.split(" ");
             }
-            """)
+            """
+          )
         );
     }
 
     @Test
-    void splitWithSplitArg() {
+    void splitWithArg() {
         rewriteRun(
-          spec -> spec.recipe(new SplitRecipe()),
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.SplitRecipe()),
           //language=java
-          java("""
+          java(
+          """
             import org.apache.commons.lang3.StringUtils;
             
             class Foo {
                 String in = "foo";
-                String[] out = StringUtils.split(in, "|");    
+                String[] out = StringUtils.split(in, ", ");
             }
-            """, """
+            """,
+          """
             class Foo {
                 String in = "foo";
-                String[] out = in.split("|");
+                String[] out = in.split(", ");
             }
-            """)
+            """
+          )
         );
     }
 
     @Test
     void equalsTest() {
         rewriteRun(
-          spec -> spec.recipe(new EqualsRecipe()),
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.EqualsRecipe()),
           //language=java
-          java("""
+          java(
+          """
             import org.apache.commons.lang3.StringUtils;
             
             class Foo {
                 String in = "foo";
                 boolean out = StringUtils.equals(in, "string");
             }
-            """, """
+            """,
+          """
             class Foo {
                 String in = "foo";
                 boolean out = in.equals("string");
             }
-            """)
+            """
+          )
         );
     }
 
-    //@Test
-    //void oneArgTwoTemplateParameters() {
-    //    rewriteRun(
-    //      spec -> spec.recipe(new IsEmptyRecipe()),
-    //      //language=java
-    //      java("""
-    //        import org.apache.commons.lang3.StringUtils;
-    //
-    //        class Foo {
-    //            String in = "foo";
-    //            String out = StringUtils.chop(in);
-    //        }
-    //        """, """
-    //        class Foo {
-    //            String in = "foo";
-    //            String out = in.substring(0, in.length() - 1);
-    //        }
-    //        """)
-    //    );
-    //}
+    @Test
+    void chopTest() {
+        // Refaster recipes does not have support for using input argument twice in template parameters
+        rewriteRun(
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.ChopRecipe()),
+          //language=java
+          java(
+          """
+            import org.apache.commons.lang3.StringUtils;
+    
+            class Foo {
+                String in = "foo";
+                String out = StringUtils.chop(in);
+            }
+            """,
+          """
+            class Foo {
+                String in = "foo";
+                String out = in.substring(0, in.length() - 1);
+            }
+            """
+          )
+        );
+    }
 
-    //@Test
-    //void outOfOrderTemplateParameters() {
-    //    rewriteRun(
-    //      spec -> spec.recipe(new IsBlankRecipe()),
-    //      //language=java
-    //      java("""
-    //        import org.apache.commons.lang3.StringUtils;
-    //
-    //        class Foo {
-    //            String in = "foo";
-    //            String suffix = "oo";
-    //            String out = StringUtils.stripEnd(in, suffix);
-    //        }
-    //        """, """
-    //        class Foo {
-    //            String in = "foo";
-    //            String suffix = "oo";
-    //            String out = in.endsWith(suffix) ? in.substring(0, in.lastIndexOf(suffix)) : in;
-    //        }
-    //        """)
-    //    );
-    //}
+    @Test
+    void replaceTest() {
+        rewriteRun(
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.ReplaceRecipe()),
+          //language=java
+          java(
+          """
+            import org.apache.commons.lang3.StringUtils;
+    
+            class Foo {
+                String in = "foo";
+                String out = StringUtils.replace(in, "o", "z");
+            }
+            """,
+          """
+            class Foo {
+                String in = "foo";
+                String out = in.replaceAll("o", "z");
+            }
+            """
+          )
+        );
+    }
 
-    //@Test
-    //void threeArguments() {
-    //    rewriteRun(
-    //      spec -> spec.recipe(new IsBlankRecipe()),
-    //      //language=java
-    //      java("""
-    //        import org.apache.commons.lang3.StringUtils;
-    //
-    //        class Foo {
-    //            String in = "foo";
-    //            String out = StringUtils.replace(in, "o", "z");
-    //        }
-    //        """, """
-    //        class Foo {
-    //            String in = "foo";
-    //            String out = in.replaceAll("o", "z");
-    //        }
-    //        """)
-    //    );
-    //}
+    @Test
+    void stripTest() {
+        rewriteRun(
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.StripRecipe()),
+          //language=java
+          java(
+            """
+              import org.apache.commons.lang3.StringUtils;
+              
+              class Foo {
+                  String in = "foo";
+                  String out = StringUtils.strip(in);
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  String out = in.trim();
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void joinTest() {
+        rewriteRun(
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.JoinRecipe()),
+          //language=java
+          java(
+            """
+              import org.apache.commons.lang3.StringUtils;
+              
+              class Foo {
+                  String in = "foo";
+                  String out = StringUtils.join(in);
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  String out = String.join(in);
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void deleteWhitespaceTest() {
+        rewriteRun(
+          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.DeleteWhitespaceRecipe()),
+          //language=java
+          java(
+            """
+              import org.apache.commons.lang3.StringUtils;
+              
+              class Foo {
+                  String in = "foo";
+                  String out = StringUtils.deleteWhitespace(in);
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  String out = in.replaceAll("\\s+", "");
+              }
+              """
+          )
+        );
+    }
+
+
 }
