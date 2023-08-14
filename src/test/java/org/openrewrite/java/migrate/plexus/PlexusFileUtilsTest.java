79f1421863e5b114d6122549a23184e4bb549c92
Tim te Beek <tim@moderne.io>
2023-08-14 20:55:10 +0200

    Split out different ways of how imports should be handled

--- src/test/java/org/openrewrite/java/migrate/plexus/PlexusFileUtilsTest.java
+++ src/test/java/org/openrewrite/java/migrate/plexus/PlexusFileUtilsTest.java
@@ -32,45 +32,31 @@ class PlexusFileUtilsTest implements RewriteTest {
     }
 
     @Test
-    void deleteDirectory() {
+    void deleteDirectoryFullyQualified() {
         rewriteRun(
-          spec -> spec.recipes(
-            new PlexusFileUtilsRecipes.DeleteDirectoryFileRecipe(),
-            new PlexusFileUtilsRecipes.DeleteDirectoryStringRecipe()
-          ),
           //language=java
           java(
             """
               import java.io.File;
-              import java.io.IOException;
-              import org.codehaus.plexus.util.FileUtils;
+                            
               class Test {
-                  void test() throws IOException {
-                      FileUtils.deleteDirectory("test");
+                  void test() throws Exception {
                       org.codehaus.plexus.util.FileUtils.deleteDirectory("test");
-
                       File file = new File("test");
-                      FileUtils.deleteDirectory(file);
                       org.codehaus.plexus.util.FileUtils.deleteDirectory(file);
-
-                      FileUtils.dirname("/foo/bar"); // Unused
                   }
               }
               """,
             """
-              import java.io.File;
-              import java.io.IOException;
               import org.apache.commons.io.FileUtils;
+                            
+              import java.io.File;
+                            
               class Test {
-                  void test() throws IOException {
+                  void test() throws Exception {
                       FileUtils.deleteDirectory(new File("test"));
-                      org.apache.commons.io.FileUtils.deleteDirectory(new File("test"));
-
                       File file = new File("test");
                       FileUtils.deleteDirectory(file);
-                      org.apache.commons.io.FileUtils.deleteDirectory(file);
-
-                      FileUtils.dirname("/foo/bar"); // Unused
                   }
               }
               """
@@ -79,32 +65,67 @@ class PlexusFileUtilsTest implements RewriteTest {
     }
 
     @Test
-    void deleteDirectoryMinimal() {
+    void deleteDirectorySimpleImport() {
         rewriteRun(
-          spec -> spec.recipes(
-            new PlexusFileUtilsRecipes.DeleteDirectoryStringRecipe()
-          ),
           //language=java
           java(
             """
+              import org.codehaus.plexus.util.FileUtils;
+                            
               import java.io.File;
-              import java.io.IOException;
+                            
               class Test {
-                  void test() throws IOException {
-                      org.codehaus.plexus.util.FileUtils.deleteDirectory("test");
+                  void test() throws Exception {
+                      FileUtils.deleteDirectory("test");
                   }
               }
               """,
             """
+              import org.apache.commons.io.FileUtils;
+                            
               import java.io.File;
-              import java.io.IOException;
+                            
               class Test {
-                  void test() throws IOException {
-                      org.apache.commons.io.FileUtils.deleteDirectory(new File("test"));
+                  void test() throws Exception {
+                      FileUtils.deleteDirectory(new File("test"));
                   }
               }
               """
           )
         );
     }
+
+    @Test
+    void deleteDirectoryRetainedImport() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.codehaus.plexus.util.FileUtils;
+                            
+              import java.io.File;
+                            
+              class Test {
+                  void test() throws Exception {
+                      FileUtils.deleteDirectory("test");
+                      FileUtils.dirname("/foo/bar");
+                  }
+              }
+              """,
+            """
+              import org.codehaus.plexus.util.FileUtils;
+                            
+              import java.io.File;
+                            
+              class Test {
+                  void test() throws Exception {
+                      org.apache.commons.io.FileUtils.deleteDirectory(new File("test"));
+                      FileUtils.dirname("/foo/bar");
+                  }
+              }
+              """
+          )
+        );
+    }
+
 }
