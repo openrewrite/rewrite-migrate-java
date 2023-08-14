fb163492797b085075d9d6372e9ffa8fd3ba936b
Tim te Beek <tim@moderne.io>
2023-08-14 18:35:45 +0200

    Always run all ApacheCommonsStringUtilsRecipes

--- src/test/java/org/openrewrite/java/migrate/apache/commons/lang/ApacheCommonsStringUtilsTest.java
+++ src/test/java/org/openrewrite/java/migrate/apache/commons/lang/ApacheCommonsStringUtilsTest.java
@@ -29,19 +29,19 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.parser(JavaParser.fromJavaVersion().classpath("commons-lang3"));
+        spec.parser(JavaParser.fromJavaVersion().classpath("commons-lang3"))
+          .recipe(new ApacheCommonsStringUtilsRecipes());
     }
 
     @Test
     @DocumentExample
     void defaultString() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.DefaultStringRecipe()),
           //language=java
           java(
             """
               import org.apache.commons.lang3.StringUtils;
-              
+                            
               class Foo {
                   String in = "foo";
                   String out = StringUtils.defaultString(in);
@@ -49,7 +49,7 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
               """,
             """
               import java.util.Objects;
-              
+                            
               class Foo {
                   String in = "foo";
                   String out = Objects.toString(in);
@@ -62,25 +62,24 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void defaultStringStatic() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.DefaultStringRecipe()),
           //language=java
           java(
-          """
-            import static org.apache.commons.lang3.StringUtils.defaultString;
-                          
-            class Foo {
-                String in = "foo";
-                String out = defaultString(in);
-            }
-            """,
-          """
-            import java.util.Objects;
-                          
-            class Foo {
-                String in = "foo";
-                String out = Objects.toString(in);
-            }
             """
+              import static org.apache.commons.lang3.StringUtils.defaultString;
+                            
+              class Foo {
+                  String in = "foo";
+                  String out = defaultString(in);
+              }
+              """,
+            """
+              import java.util.Objects;
+                            
+              class Foo {
+                  String in = "foo";
+                  String out = Objects.toString(in);
+              }
+              """
           )
         );
     }
@@ -88,23 +87,22 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void isEmpty() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.IsEmptyRecipe()),
           //language=java
           java(
-          """
-            import org.apache.commons.lang3.StringUtils;
-    
-            class Foo {
-                String in = "foo";
-                boolean out = StringUtils.isEmpty(in);
-            }
-            """,
-          """
-            class Foo {
-                String in = "foo";
-                boolean out = in == null || in.isEmpty();
-            }
             """
+              import org.apache.commons.lang3.StringUtils;
+                  
+              class Foo {
+                  String in = "foo";
+                  boolean out = StringUtils.isEmpty(in);
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  boolean out = in == null || in.isEmpty();
+              }
+              """
           )
         );
     }
@@ -112,23 +110,22 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void splitTest() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.SplitRecipe()),
           //language=java
           java(
-          """
-            import org.apache.commons.lang3.StringUtils;
-            
-            class Foo {
-                String in = "foo";
-                String[] out = StringUtils.split(in);
-            }
-            """,
-          """
-            class Foo {
-                String in = "foo";
-                String[] out = in.split(" ");
-            }
             """
+              import org.apache.commons.lang3.StringUtils;
+                          
+              class Foo {
+                  String in = "foo";
+                  String[] out = StringUtils.split(in);
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  String[] out = in.split(" ");
+              }
+              """
           )
         );
     }
@@ -137,23 +134,22 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Disabled("Argument variant not covered yet")
     void splitWithArg() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.SplitRecipe()),
           //language=java
           java(
-          """
-            import org.apache.commons.lang3.StringUtils;
-            
-            class Foo {
-                String in = "foo";
-                String[] out = StringUtils.split(in, ", ");
-            }
-            """,
-          """
-            class Foo {
-                String in = "foo";
-                String[] out = in == null ? null : in.split(", ");
-            }
             """
+              import org.apache.commons.lang3.StringUtils;
+                          
+              class Foo {
+                  String in = "foo";
+                  String[] out = StringUtils.split(in, ", ");
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  String[] out = in == null ? null : in.split(", ");
+              }
+              """
           )
         );
     }
@@ -161,50 +157,47 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void equalsTest() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.EqualsRecipe()),
           //language=java
           java(
-          """
-            import org.apache.commons.lang3.StringUtils;
-            
-            class Foo {
-                String in = "foo";
-                boolean out = StringUtils.equals(in, "string");
-            }
-            """,
-          """
-            import java.util.Objects;
-            
-            class Foo {
-                String in = "foo";
-                boolean out = Objects.equals(in, "string");
-            }
             """
+              import org.apache.commons.lang3.StringUtils;
+                          
+              class Foo {
+                  String in = "foo";
+                  boolean out = StringUtils.equals(in, "string");
+              }
+              """,
+            """
+              import java.util.Objects;
+                          
+              class Foo {
+                  String in = "foo";
+                  boolean out = Objects.equals(in, "string");
+              }
+              """
           )
         );
     }
 
     @Test
     void chopTest() {
-        // Refaster recipes does not have support for using input argument twice in template parameters
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.ChopRecipe()),
           //language=java
           java(
-          """
-            import org.apache.commons.lang3.StringUtils;
-    
-            class Foo {
-                String in = "foo";
-                String out = StringUtils.chop(in);
-            }
-            """,
-          """
-            class Foo {
-                String in = "foo";
-                String out = in.substring(0, in.length() - 1);
-            }
             """
+              import org.apache.commons.lang3.StringUtils;
+                  
+              class Foo {
+                  String in = "foo";
+                  String out = StringUtils.chop(in);
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  String out = in.substring(0, in.length() - 1);
+              }
+              """
           )
         );
     }
@@ -212,23 +205,22 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void replaceTest() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.ReplaceRecipe()),
           //language=java
           java(
-          """
-            import org.apache.commons.lang3.StringUtils;
-    
-            class Foo {
-                String in = "foo";
-                String out = StringUtils.replace(in, "o", "z");
-            }
-            """,
-          """
-            class Foo {
-                String in = "foo";
-                String out = in.replaceAll("o", "z");
-            }
             """
+              import org.apache.commons.lang3.StringUtils;
+                  
+              class Foo {
+                  String in = "foo";
+                  String out = StringUtils.replace(in, "o", "z");
+              }
+              """,
+            """
+              class Foo {
+                  String in = "foo";
+                  String out = in.replaceAll("o", "z");
+              }
+              """
           )
         );
     }
@@ -236,12 +228,11 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void stripTest() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.StripRecipe()),
           //language=java
           java(
             """
               import org.apache.commons.lang3.StringUtils;
-              
+                            
               class Foo {
                   String in = "foo";
                   String out = StringUtils.strip(in);
@@ -260,12 +251,11 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void joinTest() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.JoinRecipe()),
           //language=java
           java(
             """
               import org.apache.commons.lang3.StringUtils;
-              
+                            
               class Foo {
                   String in = "foo";
                   String out = StringUtils.join(in);
@@ -284,12 +274,11 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
     @Test
     void deleteWhitespaceTest() {
         rewriteRun(
-          spec -> spec.recipe(new ApacheCommonsStringUtilsRecipes.DeleteWhitespaceRecipe()),
           //language=java
           java(
             """
               import org.apache.commons.lang3.StringUtils;
-              
+                            
               class Foo {
                   String in = "foo";
                   String out = StringUtils.deleteWhitespace(in);
@@ -304,6 +293,4 @@ class ApacheCommonsStringUtilsTest implements RewriteTest {
           )
         );
     }
-
-
 }
