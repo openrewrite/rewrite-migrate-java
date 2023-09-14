584e6d341c0d03c39b8b0b2afd96759fdfd1ba47
Aleksandar A Simpson <alek@asu.me>
2023-09-14 12:08:41 -0700

    Added ApacheCommonsFileUtils WriteStringToFile Refaster templates (#278)
    * added CopyDirectory and WriteStringToFile refaster templates
    
    * added first two refaster templates, and created ubertest
    
    * updated license headers
    
    * Move classes; disable Write; make test compile, sorted & pass
    
    * Remove CopyDirectory for now.
    
    * Fix failing test
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: Knut Wannheden <knut@moderne.io>

--- src/main/java/org/openrewrite/java/migrate/apache/commons/io/ApacheCommonsFileUtils.java
+++ src/main/java/org/openrewrite/java/migrate/apache/commons/io/ApacheCommonsFileUtils.java
@@ -20,6 +20,7 @@ import com.google.errorprone.refaster.annotation.BeforeTemplate;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.nio.file.Files;
 
 public class ApacheCommonsFileUtils {
     private static class GetFile {
@@ -46,6 +47,19 @@ public class ApacheCommonsFileUtils {
 //            Files.write(file.toPath(), Arrays.asList(data), cs);
 //        }
 //    }
+
+    @SuppressWarnings("deprecation")
+    private static class WriteStringToFile {
+        @BeforeTemplate
+        void before(File a, String s) throws Exception {
+            FileUtils.writeStringToFile(a, s);
+        }
+
+        @AfterTemplate
+        void after(File a, String s) throws Exception {
+            Files.write(a.toPath(), s.getBytes());
+        }
+    }
 
 
 }
