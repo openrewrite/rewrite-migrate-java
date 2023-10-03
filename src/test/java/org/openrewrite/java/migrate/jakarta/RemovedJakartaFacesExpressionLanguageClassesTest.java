7d405af82d675622b9eb2a4dab136f64a14ed1b8
anuram <ranuradh@us.ibm.com>
2023-10-03 11:28:06 -0400

    adding recipe for RemovedJakartaFacesExpressionLanguageClasses jakarta_ee10

---
+++ src/test/java/org/openrewrite/java/migrate/jakarta/RemovedJakartaFacesExpressionLanguageClassesTest.java
@@ -0,0 +1,103 @@
+package org.openrewrite.java.migrate.jakarta;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+public class RemovedJakartaFacesExpressionLanguageClassesTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "jakarta.el-api-4.0.0","jakarta.faces-3.0.3"))
+          .recipe(Environment.builder()
+            .scanRuntimeClasspath("org.openrewrite.java.migrate.jakarta").build()
+            .activateRecipes("org.openrewrite.java.migrate.jakarta.RemovedJakartaFacesExpressionLanguageClasses"));
+    }
+    @Test
+    void removedExpressionLanguageTest_1() {
+        rewriteRun(
+          //language=java
+          java("""
+            package com.test;
+             
+            import jakarta.faces.el.MethodBinding;
+            import jakarta.faces.el.PropertyResolver;
+            import jakarta.faces.el.ValueBinding;
+            
+            public class Test {
+             
+                 public void testJakarta() {
+                      MethodBinding methodBinding = null;
+                      PropertyResolver propertyResolver = null;
+                      ValueBinding valueBinding = null;           
+                 }
+            }        
+            """, """
+            package com.test;
+            
+            import jakarta.el.ELResolver;
+            import jakarta.el.MethodExpression;
+            import jakarta.el.ValueExpression;
+     
+            public class Test {
+             
+                 public void testJakarta() {
+                      MethodExpression methodBinding = null;
+                      ELResolver propertyResolver = null;
+                      ValueExpression valueBinding = null;
+                 }
+            }   
+            """));
+    }
+    @Test
+    void removedExpressionLanguageTest_2() {
+        rewriteRun(
+          //language=java
+          java("""
+           package com.test;
+             
+           import jakarta.faces.el.VariableResolver;
+           import jakarta.faces.el.EvaluationException;
+           import jakarta.faces.el.MethodNotFoundException;
+           import jakarta.faces.el.PropertyNotFoundException;
+           import jakarta.faces.el.ReferenceSyntaxException;
+             
+           public class Test {
+             
+               public void testJakarta_1() {            
+                   VariableResolver variableResolver = null;
+                   EvaluationException evaluationException = null;
+                   MethodNotFoundException methodNotFoundException = null;
+                   PropertyNotFoundException propertyNotFoundException = null;
+                   ReferenceSyntaxException referenceSyntaxException = null; 
+               }
+           }         
+            """, """
+          package com.test;
+  
+          import jakarta.el.ELException;
+          import jakarta.el.ELResolver;
+          import jakarta.el.MethodNotFoundException;
+          import jakarta.el.PropertyNotFoundException;
+  
+          public class Test {
+          
+              public void testJakarta_1() {
+                  ELResolver variableResolver = null;
+                  ELException evaluationException = null;
+                  MethodNotFoundException methodNotFoundException = null;
+                  PropertyNotFoundException propertyNotFoundException = null;
+                  ELException referenceSyntaxException = null;
+              }
+          }
+          """));
+    }
+
+}
