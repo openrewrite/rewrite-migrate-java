7e1c08457edae143b035400bd4d29a19aa53c939
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-10-21 19:05:35 +0200

    chore: remove unused imports

--- src/test/java/org/openrewrite/java/migrate/jakarta/JavaxToJakartaTest.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/JavaxToJakartaTest.java
@@ -19,7 +19,6 @@ import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.config.Environment;
-import org.openrewrite.java.ChangeType;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
