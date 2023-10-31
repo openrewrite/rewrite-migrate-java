0ce34a8269e4208932f3b581316424de3c6943ad
ranuradh <93149514+ranuradh@users.noreply.github.com>
2023-10-31 08:13:31 -0400

    Adding 4 JakartaEE 10 recipes (#323)
    * adding for simple XML recipe
    
    * adding for jndi
    
    * my jndi update
    
    * BeansXmlNoVersionNowImplicitTest
    
    * adding 4 JakartaEE10 recipes
    
    * removing extra recipe  jakarta-ee-10.yml
    
    * add license for FacesJNDINamesChangedTest.java
    
    * updated jakarta-ee-10.yml
    
    * update displayName SOAPElementFactory
    
    ---------
    
    Co-authored-by: anuram <ranuradh@us.ibm.com>
    Co-authored-by: Joan Viladrosa <joan@moderne.io>

---
+++ src/test/java/org/openrewrite/java/migrate/jakarta/RemovedJakartaFacesResourceResolverTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.jakarta;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+public class RemovedJakartaFacesResourceResolverTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion().
+          classpathFromResources(new InMemoryExecutionContext(), "jakarta.faces-api-4.0.0-M6", "jakarta.faces-2.3.19", "jakarta.faces-3.0.3")).
+          recipe(Environment.builder().scanRuntimeClasspath("org.openrewrite.java.migrate.jakarta")
+            .build()
+            .activateRecipes("org.openrewrite.java.migrate.jakarta.RemovedJakartaFacesResourceResolver"));
+    }
+
+    @Test
+    void removedJakartaFacesResourceResolver_1() {
+        rewriteRun(
+          //language=java
+          java("""
+            package com.test;
+            import java.net.URL;
+               
+            import jakarta.faces.application.StateManager;
+            import jakarta.faces.component.UIViewRoot;
+            import jakarta.faces.context.FacesContext;
+            import jakarta.faces.view.facelets.ResourceResolver;
+               
+            public class ResourceResolverParent extends ResourceResolver {
+               
+                @Override
+                public URL resolveUrl(String arg0) {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+            }
+            """, """
+            package com.test;
+            import java.net.URL;
+               
+            import jakarta.faces.application.ResourceHandler;
+            import jakarta.faces.application.StateManager;
+            import jakarta.faces.component.UIViewRoot;
+            import jakarta.faces.context.FacesContext;
+               
+            public class ResourceResolverParent extends ResourceHandler {
+               
+                @Override
+                public URL resolveUrl(String arg0) {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+            }
+            """));
+    }
+
+    void removedJavaxFacesResourceResolver_1() {
+        rewriteRun(
+          //language=java
+          java("""
+                    package com.test;
+                    import java.net.URL;
+                       
+                    import javax.faces.application.StateManager;
+                    import javax.faces.component.UIViewRoot;
+                    import javax.faces.context.FacesContext;
+                    import javax.faces.view.facelets.ResourceResolver;
+                       
+                    public class ResourceResolverParent extends ResourceResolver {
+                       
+                        @Override
+                        public URL resolveUrl(String arg0) {
+                            // TODO Auto-generated method stub
+                            return null;
+                        }
+                    }
+            """, """
+                    package com.test;
+                    import java.net.URL;
+                       
+                    import jakarta.faces.application.ResourceHandler;
+                    import jakarta.faces.application.StateManager;
+                    import jakarta.faces.component.UIViewRoot;
+                    import jakarta.faces.context.FacesContext;
+                       
+                    public class ResourceResolverParent extends ResourceHandler {
+                       
+                        @Override
+                        public URL resolveUrl(String arg0) {
+                            // TODO Auto-generated method stub
+                            return null;
+                        }
+                    }
+            """));
+    }
+
+}
---
+++ src/test/java/org/openrewrite/java/migrate/jakarta/RemovedUIComponentConstantTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.jakarta;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+public class RemovedUIComponentConstantTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion().
+            classpathFromResources(new InMemoryExecutionContext(), "jakarta.faces-3.0.3")).
+          recipe(Environment.builder().scanRuntimeClasspath("org.openrewrite.java.migrate.jakarta")
+            .build()
+            .activateRecipes("org.openrewrite.java.migrate.jakarta.RemovedUIComponentConstant"));
+    }
+
+    @Test
+    void removedUIComponentConstant_1() {
+        rewriteRun(
+          //language=java
+          java("""
+            package com.test;
+            import jakarta.faces.component.UIComponent;
+             
+             public class UseUIComponentConstants {
+             
+             	public static void main(String[] args) {
+             		String str = UIComponent.CURRENT_COMPONENT;
+             		String str2 = UIComponent.CURRENT_COMPOSITE_COMPONENT;
+             		System.out.println(str);
+             		System.out.println(str2);
+             	}
+             }
+            """, """
+            package com.test;
+            import jakarta.faces.component.UIComponent;
+             
+             public class UseUIComponentConstants {
+             
+             	public static void main(String[] args) {
+             		String str = UIComponent.getCurrentComponent();
+             		String str2 = UIComponent.getCurrentCompositeComponent();
+             		System.out.println(str);
+             		System.out.println(str2);
+             	}
+             }
+            """));
+    }
+
+
+}
