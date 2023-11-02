8caa4fdda79ac604a227be7896d07cc51d6ff3c9
Knut Wannheden <knut@moderne.io>
2023-11-02 07:58:31 +0100

    Do not wrap field access in `StringFormatted`

--- src/main/java/org/openrewrite/java/migrate/lang/StringFormatted.java
+++ src/main/java/org/openrewrite/java/migrate/lang/StringFormatted.java
@@ -73,7 +73,8 @@ public class StringFormatted extends Recipe {
         private static boolean wrapperNotNeeded(Expression expression) {
             return expression instanceof J.Identifier
                     || expression instanceof J.Literal
-                    || expression instanceof J.MethodInvocation;
+                    || expression instanceof J.MethodInvocation
+                    || expression instanceof J.FieldAccess;
         }
     }
 
