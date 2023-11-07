e1072c879abf50f7be56cdf265d9171381c43a55
anuram <ranuradh@us.ibm.com>
2023-11-07 12:10:11 -0500

    remove public class from test classes

--- src/test/java/org/openrewrite/java/migrate/jakarta/DeprecatedCDIAPIsRemoved40Test.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/DeprecatedCDIAPIsRemoved40Test.java
@@ -25,7 +25,7 @@ import org.openrewrite.test.RewriteTest;
 import static org.openrewrite.java.Assertions.java;
 
 
-public class DeprecatedCDIAPIsRemoved40Test implements RewriteTest {
+ class DeprecatedCDIAPIsRemoved40Test implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec
