ac34dd2f848d64898896ce82ed51d570a064fe12
anuram <ranuradh@us.ibm.com>
2023-11-06 21:05:38 -0500

    partial cdi recipe

---
+++ src/main/java/org/openrewrite/java/migrate/jakarta/UpdateAddAnnotatedType.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.jakarta;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.lang.NonNull;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.tree.J;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class UpdateAddAnnotatedType extends Recipe {
+
+    @Option(displayName = "Method Pattern",
+            description = "A method pattern for matching required method definition.",
+            example = "*..* hello(..)")
+    @NonNull
+    String methodPattern;
+    @Override
+    public String getDisplayName() {
+        return "Replace `addAnnotatedType(AnnotatedType)` with `addAnnotatedType(AnnotatedType,String)`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "BeforeBeanDiscovery.addAnnotatedType(AnnotatedType) is Deprecated in CDI 1.1. It is Replaced by BeforeBeanDiscovery.addAnnotatedType(AnnotatedType, String).";
+    }
+
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        //TODO here check it its jakarta or javax
+        MethodMatcher METHOD_INPUT_PATTERN = new MethodMatcher(methodPattern,false);
+        return new JavaVisitor<ExecutionContext>() {
+            @Override
+            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                // TODO add comment
+                if (METHOD_INPUT_PATTERN.matches(method)) {
+                    return JavaTemplate.builder("#{any(jakarta.enterprise.inject.spi.AnnotatedType)},null")
+                            .build()
+                            .apply(
+                                    updateCursor(method),
+                                    method.getCoordinates().replaceArguments(),
+                                    method.getArguments().get(0)
+                            );
+                }
+                return super.visitMethodInvocation(method, ctx);
+            }
+        };
+    }
+}
