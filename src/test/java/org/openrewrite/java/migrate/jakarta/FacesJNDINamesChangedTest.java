340b4bb16180cb79063425aad305a63225922307
Tim te Beek <tim@moderne.io>
2023-11-16 19:41:47 +0100

    Fix formatting in tests for consistency & reuse

--- src/test/java/org/openrewrite/java/migrate/jakarta/FacesJNDINamesChangedTest.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/FacesJNDINamesChangedTest.java
@@ -31,6 +31,7 @@ public class FacesJNDINamesChangedTest implements RewriteTest {
             .build()
             .activateRecipes("org.openrewrite.java.migrate.jakarta.FacesJNDINamesChanged"));
     }
+
     @Test
     void replaceJNDI() {
         rewriteRun(
@@ -39,35 +40,34 @@ public class FacesJNDINamesChangedTest implements RewriteTest {
             """
               <?xml version="1.0" encoding="UTF-8"?>
               <web-app xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://xmlns.jcp.org/xml/ns/javaee" xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/web-app_3_1.xsd" version="3.1">
-              	<env-entry>
-              	    <env-entry-name>jsf/ClientSideSecretKey</env-entry-name>
-              	    <env-entry-type>java.lang.String</env-entry-type>
-              	    <env-entry-value>secret</env-entry-value>
-              	</env-entry>
-              	<env-entry>
-               	    <env-entry-name>jsf/FlashSecretKey</env-entry-name>
-               	    <env-entry-type>java.lang.String</env-entry-type>
-               	    <env-entry-value>secret</env-entry-value>
-               	</env-entry>
+                  <env-entry>
+                      <env-entry-name>jsf/ClientSideSecretKey</env-entry-name>
+                      <env-entry-type>java.lang.String</env-entry-type>
+                      <env-entry-value>secret</env-entry-value>
+                  </env-entry>
+                  <env-entry>
+                       <env-entry-name>jsf/FlashSecretKey</env-entry-name>
+                       <env-entry-type>java.lang.String</env-entry-type>
+                       <env-entry-value>secret</env-entry-value>
+                   </env-entry>
               </web-app>
               """,
             """
               <?xml version="1.0" encoding="UTF-8"?>
               <web-app xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://xmlns.jcp.org/xml/ns/javaee" xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/web-app_3_1.xsd" version="3.1">
-              	<env-entry>
-              	    <env-entry-name>faces/ClientSideSecretKey</env-entry-name>
-              	    <env-entry-type>java.lang.String</env-entry-type>
-              	    <env-entry-value>secret</env-entry-value>
-              	</env-entry>
-              	<env-entry>
-               	    <env-entry-name>faces/FlashSecretKey</env-entry-name>
-               	    <env-entry-type>java.lang.String</env-entry-type>
-               	    <env-entry-value>secret</env-entry-value>
-               	</env-entry>
+                  <env-entry>
+                      <env-entry-name>faces/ClientSideSecretKey</env-entry-name>
+                      <env-entry-type>java.lang.String</env-entry-type>
+                      <env-entry-value>secret</env-entry-value>
+                  </env-entry>
+                  <env-entry>
+                       <env-entry-name>faces/FlashSecretKey</env-entry-name>
+                       <env-entry-type>java.lang.String</env-entry-type>
+                       <env-entry-value>secret</env-entry-value>
+                   </env-entry>
               </web-app>
               """
           )
         );
     }
-
 }
