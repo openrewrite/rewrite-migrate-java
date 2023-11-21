276b4fe3c98efc347b833eaf4859754931260aae
Tim te Beek <tim@moderne.io>
2023-11-21 14:00:22 +0100

    Format tests to conform to project conventions

--- src/test/java/org/openrewrite/java/migrate/BeansXmlNamespaceTest.java
+++ src/test/java/org/openrewrite/java/migrate/BeansXmlNamespaceTest.java
@@ -58,19 +58,19 @@ class BeansXmlNamespaceTest implements RewriteTest {
           //language=xml
           xml(
             """
-                   <?xml version="1.0" encoding="UTF-8"?>
-                   <beans xmlns="http://xmlns.jcp.org/xml/ns/javaee" 
-                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                      xsi:schemaLocation="http://xmlns.jcp.org/DDDDxml/ns/javaee22 http://xmlns.jcp.org11/xml/ns/javaee777/beans_1_1.xsd">
-                   </beans> 
-                   """,
+              <?xml version="1.0" encoding="UTF-8"?>
+              <beans xmlns="http://xmlns.jcp.org/xml/ns/javaee" 
+                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                 xsi:schemaLocation="http://xmlns.jcp.org/DDDDxml/ns/javaee22 http://xmlns.jcp.org11/xml/ns/javaee777/beans_1_1.xsd">
+              </beans> 
+              """,
             """
-                   <?xml version="1.0" encoding="UTF-8"?>
-                   <beans xmlns="http://xmlns.jcp.org/xml/ns/javaee" 
-                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                      xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/beans_1_1.xsd">
-                   </beans> 
-                   """,
+              <?xml version="1.0" encoding="UTF-8"?>
+              <beans xmlns="http://xmlns.jcp.org/xml/ns/javaee" 
+                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                 xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/beans_1_1.xsd">
+              </beans> 
+              """,
             sourceSpecs -> sourceSpecs.path("beans.xml")
           )
         );
