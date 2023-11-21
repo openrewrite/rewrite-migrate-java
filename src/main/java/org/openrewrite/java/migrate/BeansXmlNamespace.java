79ea2faac105784d346dc2d3e68dddf98d41e580
Tim te Beek <tim@moderne.io>
2023-11-21 13:55:53 +0100

    Minimize implementation

--- src/main/java/org/openrewrite/java/migrate/BeansXmlNamespace.java
+++ src/main/java/org/openrewrite/java/migrate/BeansXmlNamespace.java
@@ -23,6 +23,10 @@ import org.openrewrite.xml.XPathMatcher;
 import org.openrewrite.xml.XmlVisitor;
 import org.openrewrite.xml.tree.Xml;
 
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+
 @Value
 @EqualsAndHashCode(callSuper = true)
 public class BeansXmlNamespace extends Recipe {
@@ -32,7 +36,6 @@ public class BeansXmlNamespace extends Recipe {
     private static final String NS_JCP = "http://xmlns.jcp.org/xml/ns/javaee";
     private static final String SUN_SCHEMA_LOCATION = "http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/beans_1_0.xsd";
     private static final String JCP_SCHEMA_LOCATION = "http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/beans_1_1.xsd";
-    private static final String DISABLE_VALIDATING = "org.jboss.weld.xml.disableValidating";
 
     @Override
     public String getDisplayName() {
@@ -46,45 +49,22 @@ public class BeansXmlNamespace extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        XmlVisitor<ExecutionContext> xmlVisitor = new XmlVisitor<ExecutionContext>() {
+        return Preconditions.check(new HasSourcePath<>("**/beans.xml"), new XmlVisitor<ExecutionContext>() {
             @Override
             public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
                 Xml.Tag t = (Xml.Tag) super.visitTag(tag, ctx);
-                if (!BEANS_MATCHER.matches(getCursor()) || t.getAttributes().stream().map(Xml.Attribute::getKeyAsString).noneMatch("xmlns"::equals)) {
-                    return t;
-                }
-                String nameSpaceValue = null, schemaLocation = null;
-                for (Xml.Attribute attribute : t.getAttributes()) {
-                    String key = attribute.getKeyAsString();
-                    String value = attribute.getValueAsString();
-                    if ("xmlns".equals(key)) {
-                        nameSpaceValue = value;
-                    } else if (key.endsWith("schemaLocation")) {
-                        schemaLocation = value;
+                if (BEANS_MATCHER.matches(getCursor())) {
+                    Map<String, String> attributes = t.getAttributes().stream().collect(toMap(Xml.Attribute::getKeyAsString, Xml.Attribute::getValueAsString));
+                    String xmlns = attributes.get("xmlns");
+                    String schemaLocation = attributes.get("xsi:schemaLocation");
+                    if (NS_SUN.equalsIgnoreCase(xmlns) && !SUN_SCHEMA_LOCATION.equalsIgnoreCase(schemaLocation)) {
+                        doAfterVisit(new ChangeTagAttribute("beans", "xsi:schemaLocation", SUN_SCHEMA_LOCATION, null).getVisitor());
+                    } else if (NS_JCP.equalsIgnoreCase(xmlns) && !JCP_SCHEMA_LOCATION.equalsIgnoreCase(schemaLocation)) {
+                        doAfterVisit(new ChangeTagAttribute("beans", "xsi:schemaLocation", JCP_SCHEMA_LOCATION, null).getVisitor());
                     }
                 }
-                if (!updateNameSpaceSchemaLocation(nameSpaceValue, schemaLocation).isEmpty()) {
-                    t = updateAttribute(t, updateNameSpaceSchemaLocation(nameSpaceValue, schemaLocation), ctx);
-                }
                 return t;
             }
-
-            private Xml.Tag updateAttribute(Xml.Tag t, String value, ExecutionContext ctx) {
-                TreeVisitor<?, ExecutionContext> changeTagVisitor = new ChangeTagAttribute("beans", "xsi:schemaLocation", value, null).getVisitor();
-                t = (Xml.Tag) changeTagVisitor.visit(t, ctx, getCursor());
-                return t;
-            }
-
-            private String updateNameSpaceSchemaLocation(String nameSpaceValue, String schemaLocation) {
-                if (NS_SUN.equalsIgnoreCase(nameSpaceValue) && !(SUN_SCHEMA_LOCATION.equalsIgnoreCase(schemaLocation))) {
-                    return (SUN_SCHEMA_LOCATION);
-                } else if (NS_JCP.equalsIgnoreCase(nameSpaceValue) && !(JCP_SCHEMA_LOCATION.equalsIgnoreCase(schemaLocation))) {
-                    return (JCP_SCHEMA_LOCATION);
-                }
-                return "";
-            }
-
-        };
-        return Preconditions.check(new HasSourcePath<>("**/beans.xml"), xmlVisitor);
+        });
     }
 }
