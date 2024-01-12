8353964499de78879f26afef78cc8c206cd0009b
Evie Lau <evie@us.ibm.com>
2024-01-12 13:29:28 -0600

    Add recipe for rule: HttpSessionInvalidate

---
+++ src/test/java/org/openrewrite/java/migrate/javaee/HttpSessionInvalidateMethodTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.javaee;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+public class HttpSessionInvalidateMethodTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "javax.servlet-3.0"))
+          .recipe(Environment.builder().scanRuntimeClasspath("org.openrewrite.java.migrate.javaee6")
+            .build().activateRecipes("org.openrewrite.java.migrate.javaee6.HttpSessionInvalidateMethod"));
+    }
+
+    @Test
+    void invalidateHttpSession() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import javax.servlet.http.HttpSession;
+
+              class Foo {
+                  void bar(HttpSession foo) {
+                      foo.invalidate();
+                  }
+              }
+              """, """
+              import javax.servlet.http.HttpServletRequest;
+
+              class Foo {
+                  void bar(HttpServletRequest foo) {
+                      foo.logout();
+                  }
+              }
+              """
+          )
+        );
+    }
+}
