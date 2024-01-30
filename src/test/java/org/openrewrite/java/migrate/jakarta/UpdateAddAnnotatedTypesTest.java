64fb5910c4423add63870610ab190d7c84702c35
Tim te Beek <tim@moderne.io>
2024-01-30 12:16:39 +0100

    Add individual unit tests to flush out issues

---
+++ src/test/java/org/openrewrite/java/migrate/jakarta/UpdateAddAnnotatedTypesTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.jakarta;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class UpdateAddAnnotatedTypesTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "jakarta.enterprise.cdi-api-3.0.0-M4", "jakarta.enterprise.cdi-api-4.0.1"))
+          .recipe(new UpdateAddAnnotatedTypes());
+    }
+
+    @Test
+    void addAnnotatedType() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              package sample.cdi;
+              
+              import jakarta.enterprise.inject.spi.AnnotatedType;
+              import jakarta.enterprise.inject.spi.BeanManager;
+              import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
+              
+              class Foo {
+                void bar(BeforeBeanDiscovery beforeBeanDiscovery, BeanManager beanManager) {
+                  AnnotatedType<String> producerType = beanManager.createAnnotatedType(String.class);
+                  beforeBeanDiscovery.addAnnotatedType(producerType); // Flag this one
+                  beforeBeanDiscovery.addAnnotatedType(producerType, "my unique id"); // Not this one
+                  beforeBeanDiscovery.addAnnotatedType(String.class, "my other unique id"); // Not this one
+                }
+              }
+              """,
+            """
+              package sample.cdi;
+              
+              import jakarta.enterprise.inject.spi.AnnotatedType;
+              import jakarta.enterprise.inject.spi.BeanManager;
+              import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
+              
+              class Foo {
+                void bar(BeforeBeanDiscovery beforeBeanDiscovery, BeanManager beanManager) {
+                  AnnotatedType<String> producerType = beanManager.createAnnotatedType(String.class);
+                  beforeBeanDiscovery.addAnnotatedType(producerType, null); // Flag this one
+                  beforeBeanDiscovery.addAnnotatedType(producerType, "my unique id"); // Not this one
+                  beforeBeanDiscovery.addAnnotatedType(String.class, "my other unique id"); // Not this one
+                }
+              }
+              """
+          )
+        );
+    }
+
+}
