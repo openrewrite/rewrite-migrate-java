2f05adecab228bb5749c9dffef1e0db00be0fd1f
Tim te Beek <tim@moderne.io>
2024-02-06 21:33:06 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/migrate/javaee/HttpSessionInvalidateTest.java
+++ src/test/java/org/openrewrite/java/migrate/javaee/HttpSessionInvalidateTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.migrate.javaee;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.migrate.javax.HttpSessionInvalidate;
