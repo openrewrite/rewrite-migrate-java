ab664613627144aebdc15083f03dce5b75eb1c40
Tim te Beek <tim@moderne.io>
2024-02-06 12:04:40 +0100

    Additional arguments for UpdateJavaCompatibility

--- src/main/java/org/openrewrite/java/migrate/UpgradeJavaVersion.java
+++ src/main/java/org/openrewrite/java/migrate/UpgradeJavaVersion.java
@@ -65,7 +65,7 @@ public class UpgradeJavaVersion extends Recipe {
                 SourceFile source = (SourceFile) tree;
 
                 if (source instanceof G.CompilationUnit && new IsBuildGradle<ExecutionContext>().visit(source, ctx) != source) {
-                    source = (SourceFile) new UpdateJavaCompatibility(version, null, null, false).getVisitor().visitNonNull(source, ctx);
+                    source = (SourceFile) new UpdateJavaCompatibility(version, null, null, false, null, null).getVisitor().visitNonNull(source, ctx);
                 } else if (source instanceof Xml.Document) {
                     source = (SourceFile) new MavenUpdateJavaVersionVisitor().visitNonNull(source, ctx);
                 }
