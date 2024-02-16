a035afd874628860bc628a4f51110c57ae308f6c
Evie Lau <evie@us.ibm.com>
2024-02-16 14:16:05 -0600

    Update test package

--- src/test/java/org/openrewrite/java/migrate/javax/AddColumnAnnotationTest.java
+++ src/test/java/org/openrewrite/java/migrate/javax/AddColumnAnnotationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.migrate.javaee;
+package org.openrewrite.java.migrate.javax;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
