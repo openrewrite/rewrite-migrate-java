7ab89a6d1ebd5737b281ac7f9ebdfd4733e4add6
Evie Lau <evie@us.ibm.com>
2024-02-20 11:03:12 -0600

    Move test to javax package

--- src/test/java/org/openrewrite/java/migrate/javax/RemoveEmbeddableIdTest.java
+++ src/test/java/org/openrewrite/java/migrate/javax/RemoveEmbeddableIdTest.java
@@ -13,13 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.migrate.javaee;
+package org.openrewrite.java.migrate.javax;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.java.JavaParser;
-import org.openrewrite.java.migrate.javax.RemoveEmbeddableId;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
