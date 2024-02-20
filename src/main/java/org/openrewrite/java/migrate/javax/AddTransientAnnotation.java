49283d85bbfe91781cf855b06227632aac0e5b7e
Evie Lau <evie@us.ibm.com>
2024-02-20 10:46:30 -0600

    Add Transient notation to unannotated Collections

---
+++ src/main/java/org/openrewrite/java/migrate/javax/AddTransientAnnotation.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.javax;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.tree.J;
+
+import java.util.regex.Pattern;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class AddTransientAnnotation extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Unannotated collection attributes require a Transient annotation";
+    }
+
+    @Override
+    public String getDescription() {
+        return "In OpenJPA, attributes that inherit from the `java.util.Collection<E>` interface are not a default " +
+               "persistent type, so these attributes are not persisted unless they are annotated. EclipseLink has a " +
+               "different default behavior and attempts to persist these attributes to the database. To keep the OpenJPA " +
+               "behavior of ignoring unannotated collection attributes, add the `javax.persistence.Transient` annotation " +
+               "to these attributes in EclipseLink.";
+    }
+
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        Pattern collection = Pattern.compile("java.util.Collection");
+        return new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext ctx) {
+                if (!multiVariable.getType().isAssignableFrom(collection)) {
+                    return multiVariable;
+                }
+                System.out.println("Pass: " + multiVariable.getTypeAsFullyQualified());
+                if (!multiVariable.getLeadingAnnotations().isEmpty()) {
+                    return multiVariable;
+                }
+                maybeAddImport("javax.persistence.Transient");
+                return JavaTemplate.builder("@Transient")
+                        .contextSensitive()
+                        .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "javax.persistence-api-2.2"))
+                        .imports("javax.persistence.Transient")
+                        .build()
+                        .apply(getCursor(), multiVariable.getCoordinates().replaceAnnotations());
+            }
+        };
+    }
+}
