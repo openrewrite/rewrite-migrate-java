48b640cfb5d72166ff2025d1f3db3c377e755900
Anu Ramamoorthy <93149514+ranuradh@users.noreply.github.com>
2024-03-11 10:42:28 -0400

    Update src/test/java/org/openrewrite/java/migrate/UpgradeToJava17Test.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/migrate/UpgradeToJava17Test.java
+++ src/test/java/org/openrewrite/java/migrate/UpgradeToJava17Test.java
@@ -536,19 +536,15 @@ class UpgradeToJava17Test implements RewriteTest {
           //language=java
           java(
             """
-             package com.test;
-
-             import java.security.cert.Certificate;
-
-             import javax.net.ssl.SSLContext;
-             import javax.net.ssl.SSLEngine;
-             import javax.net.ssl.SSLSession;
-
-             public class RemovedSSLSessionGetPeerCertificateChainMethodImplApp {
-
-                     public void test() throws Exception {
-                         SSLEngine sslEngine = SSLContext.getDefault().createSSLEngine();
-                         SSLSession session = sslEngine.getHandshakeSession();
+            package com.test;
+            import java.security.cert.Certificate;
+            import javax.net.ssl.SSLContext;
+            import javax.net.ssl.SSLEngine;
+            import javax.net.ssl.SSLSession;
+            public class RemovedSSLSessionGetPeerCertificateChainMethodImplApp {
+                    public void test() throws Exception {
+                        SSLEngine sslEngine = SSLContext.getDefault().createSSLEngine();
+                        SSLSession session = sslEngine.getHandshakeSession();
                          session.getPeerCertificateChain(); //This should trigger
                          Certificate[] certs = session.getPeerCertificates(); //This should not trigger
                      }
