cf78d9bb5e2ae9e9b266ff83f4278d57e1cd247b
Anu Ramamoorthy <93149514+ranuradh@users.noreply.github.com>
2024-03-12 14:44:22 -0400

    Update src/test/java/org/openrewrite/java/migrate/UpgradeToJava17Test.java
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- src/test/java/org/openrewrite/java/migrate/UpgradeToJava17Test.java
+++ src/test/java/org/openrewrite/java/migrate/UpgradeToJava17Test.java
@@ -554,8 +554,8 @@ class UpgradeToJava17Test implements RewriteTest {
              import javax.net.ssl.SSLContext;
              import javax.net.ssl.SSLEngine;
              import javax.net.ssl.SSLSession;
-             public class RemovedSSLSessionGetPeerCertificateChainMethodImplApp {
-                     public void test() throws Exception {
+             class RemovedSSLSessionGetPeerCertificateChainMethodImplApp {
+                     void test() throws Exception {
                           SSLEngine sslEngine = SSLContext.getDefault().createSSLEngine();
                           SSLSession session = sslEngine.getHandshakeSession();
                           session.getPeerCertificates(); //This should trigger
