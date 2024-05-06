3897b3773ec58f5a3ad5f242c4eda564a8c81c4e
Tim te Beek <tim@moderne.io>
2024-05-06 19:11:58 +0200

    Also change Guava Predicate method name (#473)
    * Also change Guava Predicate method name
    
    Fixes #435
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/test/java/org/openrewrite/java/migrate/guava/PreferJavaUtilPredicateTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.guava;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class PreferJavaUtilPredicateTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("org.openrewrite.java.migrate.guava.PreferJavaUtilPredicate")
+          .parser(JavaParser.fromJavaVersion().classpath("guava"));
+    }
+
+    @DocumentExample
+    @Test
+    void changeTypeAndMethodName() {
+        rewriteRun(
+          //language=java
+          java(
+            """   
+              import com.google.common.base.Predicate;
+
+              class A {
+                  public static Predicate<String> makeStringPredicate() {
+                      return new Predicate<String>() {
+                          @Override
+                          public boolean apply(String input) {
+                              return input.isEmpty();
+                          }
+                      };
+                  }
+              }
+              """,
+            """ 
+              import java.util.function.Predicate;
+
+              class A {
+                  public static Predicate<String> makeStringPredicate() {
+                      return new Predicate<String>() {
+                          @Override
+                          public boolean test(String input) {
+                              return input.isEmpty();
+                          }
+                      };
+                  }
+              }
+              """
+          )
+        );
+    }
+}
