d722b4644534a3de05b11aec822da6b8686c4f61
Tim te Beek <tim@moderne.io>
2024-05-10 20:31:23 +0200

    Add argument to fix compiler issue

--- src/main/java/org/openrewrite/java/migrate/javax/AddJaxwsRuntime.java
+++ src/main/java/org/openrewrite/java/migrate/javax/AddJaxwsRuntime.java
@@ -116,11 +116,11 @@ public class AddJaxwsRuntime extends Recipe {
                         Set<String> runtimeConfigurations = getTransitiveDependencyConfiguration(gp, SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT);
                         if (runtimeConfigurations.isEmpty()) {
                             if (gp.getConfiguration("compileOnly") != null) {
-                                g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, "compileOnly", null, null, null)
+                                g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, "compileOnly", null, null, null, null)
                                         .visitNonNull(g, ctx);
                             }
                             if (gp.getConfiguration("testImplementation") != null) {
-                                g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, "testImplementation", null, null, null)
+                                g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, "testImplementation", null, null, null, null)
                                         .visitNonNull(g, ctx);
                             }
                         } else {
@@ -131,7 +131,7 @@ public class AddJaxwsRuntime extends Recipe {
                                     GradleDependencyConfiguration runtimeGdc = gp.getConfiguration(runtimeConfiguration);
                                     List<GradleDependencyConfiguration> runtimeTransitives = gp.configurationsExtendingFrom(runtimeGdc, true);
                                     if (apiTransitives.stream().noneMatch(runtimeTransitives::contains)) {
-                                        g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, apiConfiguration, null, null, null)
+                                        g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, apiConfiguration, null, null, null, null)
                                                 .visitNonNull(g, ctx);
                                     }
                                 }
