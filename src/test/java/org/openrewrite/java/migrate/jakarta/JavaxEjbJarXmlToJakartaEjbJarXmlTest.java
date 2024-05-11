5c9bfec943de819540b1a4e37e0f6c4878d09059
Tim te Beek <tim@moderne.io>
2024-05-11 17:13:43 +0200

    Minor polish

--- src/test/java/org/openrewrite/java/migrate/jakarta/JavaxEjbJarXmlToJakartaEjbJarXmlTest.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/JavaxEjbJarXmlToJakartaEjbJarXmlTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2024 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ class JavaxEjbJarXmlToJakartaEjbJarXmlTest implements RewriteTest {
     }
 
     @Test
+    @DocumentExample
     void migrateJCP() {
         rewriteRun(
           //language=xml
