84d4c4be9bfb98277e3d4ed58830144a1ca1eeef
abccbaandy <abccbaandy@gmail.com>
2024-05-23 16:27:30 +0800

    add negative test

--- src/test/java/org/openrewrite/java/migrate/jakarta/JavaxToJakartaTest.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/JavaxToJakartaTest.java
@@ -500,7 +500,7 @@ class JavaxToJakartaTest implements RewriteTest {
     }
 
     @Test
-    void projectWithSpringBoot3StarterWeb() {
+    void projectWithSpringBoot3StarterWebShouldRemoveJakartaDependency() {
         rewriteRun(
           spec -> spec.parser(JavaParser.fromJavaVersion().dependsOn(javaxServlet)),
           mavenProject(
@@ -595,6 +595,45 @@ class JavaxToJakartaTest implements RewriteTest {
                   }
                   """
               )
+            )
+          )
+        );
+    }
+
+    @Test
+    void doNothingIfNotFoundTransitiveDependency() {
+        rewriteRun(
+          spec -> spec.parser(JavaParser.fromJavaVersion().dependsOn(javaxServlet)),
+          mavenProject(
+            "Sample",
+            //language=java
+            srcMainJava(
+              java(
+                """
+                                    import jakarta.servlet.A;
+                                    public class TestApplication {
+                                    }
+                  """
+              )
+            ),
+            //language=xml
+            pomXml(
+              """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>org.sample</groupId>
+                  <artifactId>sample</artifactId>
+                  <version>1.0.0</version>
+                  <dependencies>
+                    <dependency>
+                      <groupId>jakarta.annotation</groupId>
+                      <artifactId>jakarta.annotation-api</artifactId>
+                      <version>1.3.5</version>
+                    </dependency>
+                  </dependencies>
+                </project>
+                """
             )
           )
         );
