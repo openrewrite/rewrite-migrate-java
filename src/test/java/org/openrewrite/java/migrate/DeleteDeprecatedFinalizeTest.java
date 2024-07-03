44c7af1f1910814689faae8abd2a71ab447112dc
Tim te Beek <tim@moderne.io>
2024-07-03 14:30:22 +0200

    Update DeleteDeprecatedFinalizeTest.java

--- src/test/java/org/openrewrite/java/migrate/DeleteDeprecatedFinalizeTest.java
+++ src/test/java/org/openrewrite/java/migrate/DeleteDeprecatedFinalizeTest.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+
 import static org.openrewrite.java.Assertions.java;
 
 public class DeleteDeprecatedFinalizeTest implements RewriteTest {
