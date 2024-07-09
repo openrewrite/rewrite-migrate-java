3575715848727d0215a1a70e3bb93ea595ce8ca8
bhavanapidapa <bhavana.pidapa1@ibm.com>
2024-07-09 11:49:01 -0500

    Java11 Recipe InternalBindPackages

---
+++ src/test/java/org/openrewrite/java/migrate/InternalBindPackages.java
@@ -0,0 +1,52 @@
+package org.openrewrite.java.migrate;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.java.Assertions.javaVersion;
+
+class InternalBindPackages implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "sun.internal.newClass"))
+          .recipeFromResources("org.openrewrite.java.migrate.InternalBindPackages");
+    }
+
+    @Test
+    @DocumentExample
+    void internalBindPackages() {
+        rewriteRun(
+          //language=java
+          java("""
+              package com.ibm.test;
+              public class TestInternalBindPackages {
+                  public void testInternalBindPackages() {
+                      com.sun.xml.internal.bind.v2.WellKnownNamespace namespace = null;
+                namespace.hashCode();
+                      com.sun.xml.internal.bind.v2.ContextFactory contextFactory = null;
+                      contextFactory.hashCode();
+                  }
+              }
+                  """,
+            """
+              package com.ibm.test;
+              public class TestInternalBindPackages {
+                  public void testInternalBindPackages() {
+                      com.sun.xml.bind.v2.WellKnownNamespace namespace = null;
+                namespace.hashCode();
+                      com.sun.xml.bind.v2.ContextFactory contextFactory = null;
+                      contextFactory.hashCode();
+                  }
+              }
+              """
+          )
+        );
+    }
+}
