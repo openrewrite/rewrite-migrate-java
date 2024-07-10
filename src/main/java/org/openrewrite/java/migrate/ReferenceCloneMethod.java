2ba5dc8360abba5038a1b782a17297b60828591d
anuram <ranuradh@us.ibm.com>
2024-07-10 09:01:36 -0400

    Java 11 recipe remove ReferenceCloneMethod

---
+++ src/main/java/org/openrewrite/java/migrate/ReferenceCloneMethod.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.*;
+
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+class ReferenceCloneMethod extends Recipe {
+    private static final MethodMatcher REFERENCE_CLONE = new MethodMatcher("java.lang.ref.Reference clone()", true);
+
+    @Override
+    public String getDisplayName() {
+        return "Remove the use of `java.lang.ref.Reference.clone()` method";
+    }
+
+    @Override
+    public String getDescription() {
+        return "The recipe replaces any clone calls that may resolve to a `java.lang.ref.Reference.clone()` " +
+                "or any of its known subclasses: `java.lang.ref.PhantomReference`, `java.lang.ref.SoftReference`, and `java.lang.ref.WeakReference` " +
+                "with a constructor call passing in the referent and reference queue as parameters.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check( new UsesMethod<>(REFERENCE_CLONE), new JavaVisitor<ExecutionContext>() {
+                    @Override
+                    public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                        super.visitMethodInvocation(method, ctx);
+                        if (REFERENCE_CLONE.matches(method)) {
+                            Expression methodRef = method.getSelect();
+                            assert method.getSelect() != null;
+                            String argumentName = ((J.Identifier) method.getSelect()).getSimpleName();
+                            if(methodRef.getType().toString()!=null) {
+                                String templateBuilder = "new " + methodRef.getType().toString() + "(" + argumentName + ", new ReferenceQueue<>())";
+                                JavaTemplate newReference = JavaTemplate.builder(templateBuilder)
+                                        .contextSensitive()
+                                        .imports("java.lang.ref.ReferenceQueue")
+                                        .build();
+                                return newReference.apply(getCursor(), method.getCoordinates().replace());
+                            }
+                        }
+                        return method;
+                    }
+                }
+        );
+    }
+}
---
+++ src/test/java/org/openrewrite/java/migrate/ReferenceCloneMethodTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class ReferenceCloneMethodTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+    }
+
+    @Test
+    void referenceCloneRemoval() {
+        rewriteRun(
+          spec -> spec.recipe(new ReferenceCloneMethod()),
+          //language=java
+          java(
+            """         
+              import java.lang.ref.WeakReference;
+              import java.lang.ref.SoftReference;
+              import java.lang.ref.PhantomReference;
+                           
+              public class TestReference<T>{       
+                  public static void main(String[] args)throws InterruptedException,CloneNotSupportedException{
+                      WeakReference<Object> ref = new WeakReference<Object>(null);
+                      WeakReference<Object> ref1 = (WeakReference<Object>) ref.clone();
+                      SoftReference<Object> ref3 = new SoftReference<Object>(null);
+                      SoftReference<Object> ref4 = (SoftReference<Object>) ref3.clone();
+                      PhantomReference<Object> ref5 = new PhantomReference<Object>(null,null);
+                      PhantomReference<Object> ref6 = (PhantomReference<Object>) ref5.clone();
+                  }
+               }
+              """,
+            """           
+              import java.lang.ref.WeakReference;
+              import java.lang.ref.SoftReference;
+              import java.lang.ref.PhantomReference;
+              
+              public class TestReference<T>{      \s
+                  public static void main(String[] args)throws InterruptedException,CloneNotSupportedException{
+                      WeakReference<Object> ref = new WeakReference<Object>(null);
+                      WeakReference<Object> ref1 = (WeakReference<Object>) new java.lang.ref.WeakReference<java.lang.Object>(ref, new ReferenceQueue<>());
+                      SoftReference<Object> ref3 = new SoftReference<Object>(null);
+                      SoftReference<Object> ref4 = (SoftReference<Object>) new java.lang.ref.SoftReference<java.lang.Object>(ref3, new ReferenceQueue<>());
+                      PhantomReference<Object> ref5 = new PhantomReference<Object>(null,null);
+                      PhantomReference<Object> ref6 = new PhantomReference<Object>(ref5, new ReferenceQueue<>());
+                  }
+               }
+              """
+          )
+        );
+    }
+
+    @Test
+    void noCloneRemoval() {
+        rewriteRun(
+          spec -> spec.recipe(new ReferenceCloneMethod()),
+          //language=java
+          java(
+            """                   
+              public class ClonableClass implements Cloneable {
+                	public ClonableClass(int id) {      		
+                	}         
+                	@Override
+                	public Object clone() throws CloneNotSupportedException {
+                		return super.clone();
+                	}
+              }                
+              """
+          )
+        );
+    }
+}
