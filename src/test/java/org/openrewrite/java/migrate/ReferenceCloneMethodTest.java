796c6f74af94ca54e87652bc6b41c089ddc7659e
Anu Ramamoorthy <93149514+ranuradh@users.noreply.github.com>
2024-07-10 09:14:28 -0400

    Update src/test/java/org/openrewrite/java/migrate/ReferenceCloneMethodTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/migrate/ReferenceCloneMethodTest.java
+++ src/test/java/org/openrewrite/java/migrate/ReferenceCloneMethodTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.migrate;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
