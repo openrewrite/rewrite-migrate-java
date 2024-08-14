57c90cb443dfe6c323d897fea0fcc01d1a37726b
Tim te Beek <tim@moderne.io>
2024-08-14 23:35:22 +0200

    Fix compilation and other suggested fixes

--- src/main/java/org/openrewrite/java/migrate/ReplaceLocalizedStreamMethods.java
+++ src/main/java/org/openrewrite/java/migrate/ReplaceLocalizedStreamMethods.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.migrate;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Option;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.tree.Expression;
@@ -19,12 +36,22 @@ public class ReplaceLocalizedStreamMethods extends Recipe {
     @Option(displayName = "Method pattern to replace",
             description = "The method pattern to match and replace.",
             example = "java.lang.Runtime getLocalizedInputStream(java.io.InputStream)")
-    String localizedInputStreamMethodMatcher = "java.lang.Runtime getLocalizedInputStream(java.io.InputStream)";
+    String localizedInputStreamMethodMatcher;
 
     @Option(displayName = "Method pattern to replace",
             description = "The method pattern to match and replace.",
             example = "java.lang.Runtime getLocalizedOutputStream(java.io.OutputStream)")
-    String localizedOutputStreamMethodMatcher = "java.lang.Runtime getLocalizedOutputStream(java.io.OutputStream)";
+    String localizedOutputStreamMethodMatcher;
+
+    @JsonCreator
+    public ReplaceLocalizedStreamMethods(
+            @Nullable String localizedInputStreamMethodMatcher,
+            @Nullable String localizedOutputStreamMethodMatcher) {
+        this.localizedInputStreamMethodMatcher = localizedInputStreamMethodMatcher == null ?
+                "java.lang.Runtime getLocalizedInputStream(java.io.InputStream)" : localizedInputStreamMethodMatcher;
+        this.localizedOutputStreamMethodMatcher = localizedOutputStreamMethodMatcher == null ?
+                "java.lang.Runtime getLocalizedOutputStream(java.io.OutputStream)" : localizedOutputStreamMethodMatcher;
+    }
 
     @Override
     public String getDisplayName() {
--- src/test/java/org/openrewrite/java/migrate/ReplaceLocalizedStreamMethodsTest.java
+++ src/test/java/org/openrewrite/java/migrate/ReplaceLocalizedStreamMethodsTest.java
@@ -1,36 +1,52 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.migrate;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class ReplaceLocalizedStreamMethodsTest implements RewriteTest {
-    //language=java
-    String LocalizedStreamMethodsClass = """
-           
-            package com.test;
-            import java.io.InputStream;
-            import java.io.OutputStream;
-
-            public class Runtime1 {
-
-             public InputStream getLocalizedInputStream1(InputStream in) {
-                return in;
-             }
-            
-            public OutputStream getLocalizedOutputStream1(OutputStream out) {
-                return out;
-           }
-            
-      }
-           """;
+class ReplaceLocalizedStreamMethodsTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.recipe(new ReplaceLocalizedStreamMethods("com.test.Runtime1 getLocalizedInputStream1(java.io.InputStream)", "com.test.Runtime1 getLocalizedOutputStream1(java.io.OutputStream)"));
+        spec
+          .parser(JavaParser.fromJavaVersion().dependsOn(
+            //language=java
+            """
+              package com.test;
+              import java.io.InputStream;
+              import java.io.OutputStream;
+              public class Runtime1 {
+                  public InputStream getLocalizedInputStream1(InputStream in) {
+                      return in;
+                  }
+                  public OutputStream getLocalizedOutputStream1(OutputStream out) {
+                      return out;
+                  }
+              }
+              """
+          ))
+          .recipe(new ReplaceLocalizedStreamMethods(
+            "com.test.Runtime1 getLocalizedInputStream1(java.io.InputStream)",
+            "com.test.Runtime1 getLocalizedOutputStream1(java.io.OutputStream)"));
     }
 
     @Test
@@ -38,12 +54,11 @@ public class ReplaceLocalizedStreamMethodsTest implements RewriteTest {
     void replaceGetLocalizedInputStream() {
         rewriteRun(
           //language=java
-          java(LocalizedStreamMethodsClass),
           java(
             """
               package com.test;
               import java.io.InputStream;
-                                
+
               class Test {
                   void exampleMethod(InputStream in) {
                       Runtime1 rt = null;
@@ -54,7 +69,7 @@ public class ReplaceLocalizedStreamMethodsTest implements RewriteTest {
             """
               package com.test;
               import java.io.InputStream;
-                                
+
               class Test {
                   void exampleMethod(InputStream in) {
                       Runtime1 rt = null;
@@ -70,12 +85,11 @@ public class ReplaceLocalizedStreamMethodsTest implements RewriteTest {
     void replaceGetLocalizedOutputStream() {
         rewriteRun(
           //language=java
-          java(LocalizedStreamMethodsClass),
           java(
             """
               package com.test;
               import java.io.OutputStream;
-                        
+
               class Test {
                   void exampleMethod(OutputStream out) {
                       Runtime1 rt = null;
@@ -86,7 +100,7 @@ public class ReplaceLocalizedStreamMethodsTest implements RewriteTest {
             """
               package com.test;
               import java.io.OutputStream;
-                        
+
               class Test {
                   void exampleMethod(OutputStream out) {
                       Runtime1 rt = null;
@@ -98,4 +112,3 @@ public class ReplaceLocalizedStreamMethodsTest implements RewriteTest {
         );
     }
 }
-
