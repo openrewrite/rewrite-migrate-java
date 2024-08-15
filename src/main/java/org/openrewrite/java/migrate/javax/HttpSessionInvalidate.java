cdb00e55ef31d2a46dd0d0011fbc36a180e9f970
Jonathan Schnéider <jkschneider@gmail.com>
2024-08-15 04:18:33 +0000

    Migrate to JSpecify from OpenRewrite JSR-305 meta-annotations
    Use this link to re-run the recipe: https://app.moderne.io/builder/gW3xaSLWV?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/java/migrate/javax/HttpSessionInvalidate.java
+++ src/main/java/org/openrewrite/java/migrate/javax/HttpSessionInvalidate.java
@@ -17,11 +17,11 @@ package org.openrewrite.java.migrate.javax;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.jspecify.annotations.Nullable;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
-import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.*;
 import org.openrewrite.java.search.UsesMethod;
 import org.openrewrite.java.search.UsesType;
