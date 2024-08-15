f513c9b43fc9e28ab83f79c48f2eabe09d69b8bb
Tim te Beek <tim@moderne.io>
2024-08-15 21:07:39 +0200

    Suppress warning

--- src/main/java/org/openrewrite/java/migrate/guava/NoGuavaRefaster.java
+++ src/main/java/org/openrewrite/java/migrate/guava/NoGuavaRefaster.java
@@ -63,6 +63,7 @@ public class NoGuavaRefaster {
     )
     public static class StringValueOfString {
         @BeforeTemplate
+        @SuppressWarnings("UnnecessaryCallToStringValueOf")
         String before(String string) {
             return String.valueOf(string);
         }
