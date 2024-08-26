7ad86f74544738d19a4b2f1b9746b4f9b8860c16
sullis <github@seansullivan.com>
2024-08-26 15:26:28 -0700

    Update src/test/java/org/openrewrite/java/migrate/jspecify/MigrateToJspecifyTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/migrate/jspecify/MigrateToJspecifyTest.java
+++ src/test/java/org/openrewrite/java/migrate/jspecify/MigrateToJspecifyTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.migrate.jspecify;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.config.Environment;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
