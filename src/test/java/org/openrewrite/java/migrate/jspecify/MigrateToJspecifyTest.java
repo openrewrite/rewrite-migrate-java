74df793ee73f25f201fd5b99a0d970a39a2bbf58
Tim te Beek <tim@moderne.io>
2024-08-30 11:08:17 +0200

    Scan for the old annotations when adding jspecify dependency

--- src/test/java/org/openrewrite/java/migrate/jspecify/MigrateToJspecifyTest.java
+++ src/test/java/org/openrewrite/java/migrate/jspecify/MigrateToJspecifyTest.java
@@ -21,9 +21,8 @@ import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.java.Assertions.*;
 import static org.openrewrite.maven.Assertions.pomXml;
-
 
 class MigrateToJspecifyTest implements RewriteTest {
 
@@ -37,267 +36,274 @@ class MigrateToJspecifyTest implements RewriteTest {
     @DocumentExample
     @Test
     void migrateFromJavaxAnnotationApiToJspecify() {
-        //language=java
         rewriteRun(
-          pomXml(
+          mavenProject("foo",
+            srcMainJava(
+              //language=java
+              java(
+                """
+                  import javax.annotation.Nonnull;
+                  import javax.annotation.Nullable;
+                  
+                  public class Test {
+                      @Nonnull
+                      public String field1;
+                      @Nullable
+                      public String field2;
+                      @Nullable
+                      public Foo.Bar foobar;
+                  }
+                  
+                  interface Foo {
+                    class Bar {
+                      @Nonnull
+                      public String barField;
+                    }
+                  }
+                  """,
+                """
+                  import org.jspecify.annotations.NonNull;
+                  import org.jspecify.annotations.Nullable;
+                  
+                  public class Test {
+                      @NonNull
+                      public String field1;
+                      @Nullable
+                      public String field2;
+                  
+                      public Foo.@Nullable Bar foobar;
+                  }
+                  
+                  interface Foo {
+                    class Bar {
+                      @NonNull
+                      public String barField;
+                    }
+                  }
+                  """
+              )
+            ),
             //language=xml
-            """
-              <project>
-                  <modelVersion>4.0.0</modelVersion>
-                  <groupId>com.example.foobar</groupId>
-                  <artifactId>foobar-core</artifactId>
-                  <version>1.0.0</version>
-                  <dependencies>
-                      <dependency>
-                          <groupId>javax.annotation</groupId>
-                          <artifactId>javax.annotation-api</artifactId>
-                          <version>1.3.2</version>
-                      </dependency>
-                  </dependencies>
-              </project>
-              """,
-            //language=xml
-            """
-              <project>
-                  <modelVersion>4.0.0</modelVersion>
-                  <groupId>com.example.foobar</groupId>
-                  <artifactId>foobar-core</artifactId>
-                  <version>1.0.0</version>
-                  <dependencies>
-                      <dependency>
-                          <groupId>javax.annotation</groupId>
-                          <artifactId>javax.annotation-api</artifactId>
-                          <version>1.3.2</version>
-                      </dependency>
-                      <dependency>
-                          <groupId>org.jspecify</groupId>
-                          <artifactId>jspecify</artifactId>
-                          <version>1.0.0</version>
-                      </dependency>
-                  </dependencies>
-              </project>
+            pomXml(
               """
-          ),
-          //language=java
-          java(
-            """
-              import javax.annotation.Nonnull;
-              import javax.annotation.Nullable;
-              
-              public class Test {
-                  @Nonnull
-                  public String field1;
-                  @Nullable
-                  public String field2;
-                  @Nullable
-                  public Foo.Bar foobar;
-              }
-
-              interface Foo {
-                class Bar {
-                  @Nonnull
-                  public String barField;
-                }
-              }
-              """,
-            """
-              import org.jspecify.annotations.NonNull;
-              import org.jspecify.annotations.Nullable;
-              
-              public class Test {
-                  @NonNull
-                  public String field1;
-                  @Nullable
-                  public String field2;
-
-                  public Foo.@Nullable Bar foobar;
-              }
-
-              interface Foo {
-                class Bar {
-                  @NonNull
-                  public String barField;
-                }
-              }
+                <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.example.foobar</groupId>
+                    <artifactId>foobar-core</artifactId>
+                    <version>1.0.0</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>javax.annotation</groupId>
+                            <artifactId>javax.annotation-api</artifactId>
+                            <version>1.3.2</version>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """,
               """
+                <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.example.foobar</groupId>
+                    <artifactId>foobar-core</artifactId>
+                    <version>1.0.0</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>javax.annotation</groupId>
+                            <artifactId>javax.annotation-api</artifactId>
+                            <version>1.3.2</version>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.jspecify</groupId>
+                            <artifactId>jspecify</artifactId>
+                            <version>1.0.0</version>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """
+            )
           )
         );
     }
 
     @Test
     void migrateFromJakartaAnnotationApiToJspecify() {
-        //language=java
         rewriteRun(
-          pomXml(
+          mavenProject("foo",
+            //language=java
+            srcMainJava(
+              java(
+                """
+                  import jakarta.annotation.Nonnull;
+                  import jakarta.annotation.Nullable;
+                  
+                  public class Test {
+                      @Nonnull
+                      public String field1;
+                      @Nullable
+                      public String field2;
+                      @Nullable
+                      public Foo.Bar foobar;
+                  }
+                  
+                  interface Foo {
+                    class Bar {
+                      @Nonnull
+                      public String barField;
+                    }
+                  }
+                  """,
+                """
+                  import org.jspecify.annotations.NonNull;
+                  import org.jspecify.annotations.Nullable;
+                  
+                  public class Test {
+                      @NonNull
+                      public String field1;
+                      @Nullable
+                      public String field2;
+                  
+                      public Foo.@Nullable Bar foobar;
+                  }
+                  
+                  interface Foo {
+                    class Bar {
+                      @NonNull
+                      public String barField;
+                    }
+                  }
+                  """
+              )
+            )
+            ,
             //language=xml
-            """
-              <project>
-                  <modelVersion>4.0.0</modelVersion>
-                  <groupId>com.example.foobar</groupId>
-                  <artifactId>foobar-core</artifactId>
-                  <version>1.0.0</version>
-                  <dependencies>
-                      <dependency>
-                          <groupId>jakarta.annotation</groupId>
-                          <artifactId>jakarta.annotation-api</artifactId>
-                          <version>3.0.0</version>
-                      </dependency>
-                  </dependencies>
-              </project>
-              """,
-            //language=xml
-            """
-              <project>
-                  <modelVersion>4.0.0</modelVersion>
-                  <groupId>com.example.foobar</groupId>
-                  <artifactId>foobar-core</artifactId>
-                  <version>1.0.0</version>
-                  <dependencies>
-                      <dependency>
-                          <groupId>jakarta.annotation</groupId>
-                          <artifactId>jakarta.annotation-api</artifactId>
-                          <version>3.0.0</version>
-                      </dependency>
-                      <dependency>
-                          <groupId>org.jspecify</groupId>
-                          <artifactId>jspecify</artifactId>
-                          <version>1.0.0</version>
-                      </dependency>
-                  </dependencies>
-              </project>
+            pomXml(
               """
-          ),
-          //language=java
-          java(
-            """
-              import jakarta.annotation.Nonnull;
-              import jakarta.annotation.Nullable;
-              
-              public class Test {
-                  @Nonnull
-                  public String field1;
-                  @Nullable
-                  public String field2;
-                  @Nullable
-                  public Foo.Bar foobar;
-              }
-
-              interface Foo {
-                class Bar {
-                  @Nonnull
-                  public String barField;
-                }
-              }
-              """,
-            """
-              import org.jspecify.annotations.NonNull;
-              import org.jspecify.annotations.Nullable;
-              
-              public class Test {
-                  @NonNull
-                  public String field1;
-                  @Nullable
-                  public String field2;
-
-                  public Foo.@Nullable Bar foobar;
-              }
-
-              interface Foo {
-                class Bar {
-                  @NonNull
-                  public String barField;
-                }
-              }
+                <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.example.foobar</groupId>
+                    <artifactId>foobar-core</artifactId>
+                    <version>1.0.0</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>jakarta.annotation</groupId>
+                            <artifactId>jakarta.annotation-api</artifactId>
+                            <version>3.0.0</version>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """,
               """
+                <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.example.foobar</groupId>
+                    <artifactId>foobar-core</artifactId>
+                    <version>1.0.0</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>jakarta.annotation</groupId>
+                            <artifactId>jakarta.annotation-api</artifactId>
+                            <version>3.0.0</version>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.jspecify</groupId>
+                            <artifactId>jspecify</artifactId>
+                            <version>1.0.0</version>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """
+            )
           )
         );
     }
 
     @Test
     void migrateFromJetbrainsAnnotationsToJspecify() {
-        //language=java
         rewriteRun(
-          pomXml(
+          mavenProject("foo",
+            //language=java
+            srcMainJava(
+              java(
+                """
+                  import org.jetbrains.annotations.NotNull;
+                  import org.jetbrains.annotations.Nullable;
+                  
+                  public class Test {
+                      @NotNull
+                      public String field1;
+                      @Nullable
+                      public String field2;
+                      @Nullable
+                      public Foo.Bar foobar;
+                  }
+                  
+                  interface Foo {
+                    class Bar {
+                      @NotNull
+                      public String barField;
+                    }
+                  }
+                  """,
+                """
+                  import org.jspecify.annotations.NonNull;
+                  import org.jspecify.annotations.Nullable;
+                  
+                  public class Test {
+                      @NonNull
+                      public String field1;
+                      @Nullable
+                      public String field2;
+                  
+                      public Foo.@Nullable Bar foobar;
+                  }
+                  
+                  interface Foo {
+                    class Bar {
+                      @NonNull
+                      public String barField;
+                    }
+                  }
+                  """
+              )
+            ),
             //language=xml
-            """
-              <project>
-                  <modelVersion>4.0.0</modelVersion>
-                  <groupId>com.example.foobar</groupId>
-                  <artifactId>foobar-core</artifactId>
-                  <version>1.0.0</version>
-                  <dependencies>
-                      <dependency>
-                          <groupId>org.jetbrains</groupId>
-                          <artifactId>annotations</artifactId>
-                          <version>24.1.0</version>
-                      </dependency>
-                  </dependencies>
-              </project>
-              """,
-            //language=xml
-            """
-              <project>
-                  <modelVersion>4.0.0</modelVersion>
-                  <groupId>com.example.foobar</groupId>
-                  <artifactId>foobar-core</artifactId>
-                  <version>1.0.0</version>
-                  <dependencies>
-                      <dependency>
-                          <groupId>org.jetbrains</groupId>
-                          <artifactId>annotations</artifactId>
-                          <version>24.1.0</version>
-                      </dependency>
-                      <dependency>
-                          <groupId>org.jspecify</groupId>
-                          <artifactId>jspecify</artifactId>
-                          <version>1.0.0</version>
-                      </dependency>
-                  </dependencies>
-              </project>
+            pomXml(
               """
-          ),
-          //language=java
-          java(
-            """
-              import org.jetbrains.annotations.NotNull;
-              import org.jetbrains.annotations.Nullable;
-              
-              public class Test {
-                  @NotNull
-                  public String field1;
-                  @Nullable
-                  public String field2;
-                  @Nullable
-                  public Foo.Bar foobar;
-              }
-
-              interface Foo {
-                class Bar {
-                  @NotNull
-                  public String barField;
-                }
-              }
-              """,
-            """
-              import org.jspecify.annotations.NonNull;
-              import org.jspecify.annotations.Nullable;
-              
-              public class Test {
-                  @NonNull
-                  public String field1;
-                  @Nullable
-                  public String field2;
-
-                  public Foo.@Nullable Bar foobar;
-              }
-
-              interface Foo {
-                class Bar {
-                  @NonNull
-                  public String barField;
-                }
-              }
+                <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.example.foobar</groupId>
+                    <artifactId>foobar-core</artifactId>
+                    <version>1.0.0</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.jetbrains</groupId>
+                            <artifactId>annotations</artifactId>
+                            <version>24.1.0</version>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """,
               """
+                <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.example.foobar</groupId>
+                    <artifactId>foobar-core</artifactId>
+                    <version>1.0.0</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.jetbrains</groupId>
+                            <artifactId>annotations</artifactId>
+                            <version>24.1.0</version>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.jspecify</groupId>
+                            <artifactId>jspecify</artifactId>
+                            <version>1.0.0</version>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """
+            )
           )
         );
     }
