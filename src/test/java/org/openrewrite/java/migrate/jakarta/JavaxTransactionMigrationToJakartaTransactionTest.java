0ee41df40d89b280d93c2c081fd31ebcefb62ba1
Andreas Novak <andreasnovak@hotmail.de>
2024-09-13 15:41:33 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>

--- src/test/java/org/openrewrite/java/migrate/jakarta/JavaxTransactionMigrationToJakartaTransactionTest.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/JavaxTransactionMigrationToJakartaTransactionTest.java
@@ -1,7 +1,24 @@
-package org.openrewrite.java.migrate.jakarta;
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ package org.openrewrite.java.migrate.jakarta;
 
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
+
+import org.openrewrite.DocumentExample;
 import org.openrewrite.config.Environment;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
@@ -49,7 +66,8 @@ public class JavaxTransactionMigrationToJakartaTransactionTest implements Rewrit
         rewriteRun(
           spec -> spec.parser(JavaParser.fromJavaVersion().dependsOn(javax_transaction_xa)),
           //language=java
-          java("""
+          java(
+                """
             import javax.transaction.xa.*;
             public class A {
                 XAResource xa;
@@ -58,12 +76,14 @@ public class JavaxTransactionMigrationToJakartaTransactionTest implements Rewrit
         );
     }
 
+    @DocumentExample
     @Test
     void changeImportWhenPackageFromJakartaTransaction() {
         rewriteRun(
           spec -> spec.parser(JavaParser.fromJavaVersion().dependsOn(javax_transaction, jakarta_transaction)),
           //language=java
-          java("""
+          java(
+              """
               import javax.transaction.Transactional;
               @Transactional
               public class A {
