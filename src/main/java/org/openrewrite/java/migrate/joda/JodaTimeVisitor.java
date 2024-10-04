f12381c385e76d8fa750e3470a20768cc8e74b30
amishra-u <119983081+amishra-u@users.noreply.github.com>
2024-10-04 12:16:20 -0700

    Remove duplicate license headers (#568)
    * [1/x] Implement joda to java time migration recipe
    
    * Update src/main/java/org/openrewrite/java/migrate/joda/templates/DateTimeFormatTemplates.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/main/java/org/openrewrite/java/migrate/joda/templates/DateTimeFormatTemplates.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/main/java/org/openrewrite/java/migrate/joda/templates/DurationTemplates.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/main/java/org/openrewrite/java/migrate/joda/templates/DateTimeTemplates.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/main/java/org/openrewrite/java/migrate/joda/templates/MethodTemplate.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/main/java/org/openrewrite/java/migrate/joda/templates/TimeClassNames.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/main/java/org/openrewrite/java/migrate/joda/templates/TimeZoneTemplates.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/test/java/org/openrewrite/java/migrate/joda/JodaTimeVisitorTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/test/java/org/openrewrite/java/migrate/joda/JodaTimeVisitorTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/test/java/org/openrewrite/java/migrate/joda/JodaTimeVisitorTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * remove duplicate license header
    
    * remove unused import
    
    * duplicate import
    
    ---------
    
    Co-authored-by: Sam Snyder <sam@moderne.io>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/java/migrate/joda/JodaTimeVisitor.java
+++ src/main/java/org/openrewrite/java/migrate/joda/JodaTimeVisitor.java
@@ -22,8 +22,6 @@ import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.migrate.joda.templates.*;
-import org.openrewrite.java.migrate.joda.templates.MethodTemplate;
-import org.openrewrite.java.migrate.joda.templates.TimeZoneTemplates;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaType;
