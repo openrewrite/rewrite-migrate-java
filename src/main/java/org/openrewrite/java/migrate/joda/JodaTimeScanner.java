10a2601dd6ac01ccfde123b4c4dcf9440587ab87
Anshuman Mishra <a.mishra@uber.com>
2024-11-16 15:31:06 -0800

    Formatting

--- src/main/java/org/openrewrite/java/migrate/joda/JodaTimeScanner.java
+++ src/main/java/org/openrewrite/java/migrate/joda/JodaTimeScanner.java
@@ -247,8 +247,8 @@ public class JodaTimeScanner extends ScopeAwareVisitor {
             Cursor cursor = getCursor();
             while (cursor.getValue() instanceof Expression && isJodaExpr(cursor.getValue())) {
                 Cursor parentCursor = cursor.getParentTreeCursor();
-                if (parentCursor.getValue() instanceof MethodCall
-                        && ((MethodCall) parentCursor.getValue()).getArguments().contains(cursor.getValue())) {
+                if (parentCursor.getValue() instanceof MethodCall &&
+                        ((MethodCall) parentCursor.getValue()).getArguments().contains(cursor.getValue())) {
                     return Optional.of(cursor);
                 }
                 cursor = parentCursor;
