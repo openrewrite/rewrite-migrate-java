ac12319d827e02a5506f85b08aa96635281e2ffa
timo <1398557+timo-a@users.noreply.github.com>
2024-12-15 20:11:51 +0100

    bring back original helper methods

--- src/main/java/org/openrewrite/java/migrate/lombok/LombokUtils.java
+++ src/main/java/org/openrewrite/java/migrate/lombok/LombokUtils.java
@@ -21,6 +21,10 @@ import org.openrewrite.internal.StringUtils;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.Statement;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static lombok.AccessLevel.*;
 import static org.openrewrite.java.tree.J.Modifier.Type.*;
@@ -129,6 +133,43 @@ class LombokUtils {
 
     private static boolean hasMatchingSetterMethodName(J.MethodDeclaration method, String simpleName) {
         return method.getSimpleName().equals("set" + StringUtils.capitalize(simpleName));
+    }
+
+    public static boolean isEffectivelySetter(J.MethodDeclaration method) {
+        boolean isVoid = "void".equals(method.getType().toString());
+        List<Statement> actualParameters = method.getParameters().stream()
+                .filter(s -> !(s instanceof J.Empty))
+                .collect(Collectors.toList());
+        boolean oneParam = actualParameters.size() == 1;
+        if (!isVoid || !oneParam)
+            return false;
+
+        J.VariableDeclarations variableDeclarations = (J.VariableDeclarations) actualParameters.get(0);
+        J.VariableDeclarations.NamedVariable param = variableDeclarations.getVariables().get(0);
+        String paramName = param.getName().toString();
+
+        boolean singularStatement = method.getBody() != null //abstract methods can be null
+                && method.getBody().getStatements().size() == 1
+                && method.getBody().getStatements().get(0) instanceof J.Assignment;
+
+        if (!singularStatement) {
+            return false;
+        }
+        J.Assignment assignment = (J.Assignment) method.getBody().getStatements().get(0);
+
+        J.FieldAccess fieldAccess = (J.FieldAccess) assignment.getVariable();
+
+        return
+                // assigned value is exactly the parameter
+                assignment.getAssignment().toString().equals(paramName)
+
+                        // type of parameter and field have to match
+                        && param.getType().equals(fieldAccess.getType());
+
+    }
+
+    public static String deriveSetterMethodName(JavaType.Variable fieldType) {
+        return "set" + StringUtils.capitalize(fieldType.getName());
     }
 
     static AccessLevel getAccessLevel(J.MethodDeclaration methodDeclaration) {
