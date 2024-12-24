88cec87562fd6990982fd0a7ce94ad4043ffd534
Niels de Bruin <nielsdebruin@gmail.com>
2024-12-24 14:54:07 +0100

    Update src/test/java/org/openrewrite/java/migrate/UpdateSdkManTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/migrate/UpdateSdkManTest.java
+++ src/test/java/org/openrewrite/java/migrate/UpdateSdkManTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.migrate;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.test.SourceSpecs.text;
