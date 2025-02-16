6c933b70c08975f8ba03a51b5290370a95f611e0
timo <1398557+timo-a@users.noreply.github.com>
2025-02-16 16:56:08 +0100

    Inline MethodRecorder & rename list of methods not renamed

--- src/main/java/org/openrewrite/java/migrate/lombok/NormalizeSetter.java
+++ src/main/java/org/openrewrite/java/migrate/lombok/NormalizeSetter.java
@@ -16,7 +16,6 @@
 package org.openrewrite.java.migrate.lombok;
 
 import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.ExecutionContext;
@@ -30,12 +29,13 @@ import org.openrewrite.java.tree.JavaType;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 @Value
 @EqualsAndHashCode(callSuper = false)
 public class NormalizeSetter extends ScanningRecipe<NormalizeSetter.MethodAcc> {
+
+    private final static String DO_NOT_RENAME = "DO_NOT_RENAME";
 
     @Override
     public String getDisplayName() {
@@ -45,25 +45,15 @@ public class NormalizeSetter extends ScanningRecipe<NormalizeSetter.MethodAcc> {
     @Override
     public String getDescription() {
         return "Rename methods that are effectively setter to the name lombok would give them.\n" +
-               "Limitations:\n" +
-               " - If two methods in a class are effectively the same setter then one's name will be corrected and the others name will be left as it is.\n" +
-               " - If the correct name for a method is already taken by another method then the name will not be corrected.\n" +
-               " - Method name swaps or circular renaming within a class cannot be performed because the names block each other. " +
-               "E.g. `int getFoo() { return ba; } int getBa() { return foo; }` stays as it is.";
+                "Limitations:\n" +
+                " - If two methods in a class are effectively the same setter then one's name will be corrected and the others name will be left as it is.\n" +
+                " - If the correct name for a method is already taken by another method then the name will not be corrected.\n" +
+                " - Method name swaps or circular renaming within a class cannot be performed because the names block each other. " +
+                "E.g. `int getFoo() { return ba; } int getBa() { return foo; }` stays as it is.";
     }
 
-    public static class MethodAcc  {
+    public static class MethodAcc {
         List<RenameRecord> renameRecords = new ArrayList<>();
-    }
-
-    @Override
-    public MethodAcc getInitialValue(ExecutionContext ctx) {
-        return new MethodAcc();
-    }
-
-    @Override
-    public TreeVisitor<?, ExecutionContext> getScanner(MethodAcc acc) {
-        return new MethodRecorder(acc);
     }
 
     @Value
@@ -74,99 +64,101 @@ public class NormalizeSetter extends ScanningRecipe<NormalizeSetter.MethodAcc> {
         String newMethodName_;
     }
 
-
-    @RequiredArgsConstructor
-    private static class MethodRecorder extends JavaIsoVisitor<ExecutionContext> {
-
-        private final static String METHOD_BLACKLIST = "METHOD_BLACKLIST";
-
-        private final MethodAcc acc;
-
-        @Override
-        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-
-            List<String> blackList = classDecl.getBody().getStatements().stream()
-                    .filter(s -> s instanceof J.MethodDeclaration)
-                    .map(s -> (J.MethodDeclaration) s)
-                    .map(J.MethodDeclaration::getSimpleName)
-                    .collect(Collectors.toList());
-
-            getCursor().putMessage(METHOD_BLACKLIST, blackList);
-
-            super.visitClassDeclaration(classDecl, ctx);
-
-            return classDecl;
-        }
-
-        @Override
-        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
-            assert method.getMethodType() != null;
-
-            if (!LombokUtils.isEffectivelySetter(method)) {
-                return method;
-            }
-
-            // return early if the method overrides another
-            // if the project defined both the original and the overridden method,
-            // then the renaming of the "original" in the base class will cover the override
-            if (method.getLeadingAnnotations().stream().anyMatch(a -> "Override".equals(a.getSimpleName()))) {
-                return method;
-            }
-
-            JavaType.Variable fieldType = extractVariable(method);
-
-            String expectedMethodName = LombokUtils.deriveSetterMethodName(fieldType);
-            String parameterType =  fieldType.getType().toString();
-            String actualMethodName = method.getSimpleName();
-
-            // If method already has the name it should have, then nothing to be done
-            if (expectedMethodName.equals(actualMethodName)) {
-                return method;
-            }
-
-            // If the desired method name is already taken by an existing method, the current method cannot be renamed
-            List<String> blackList = getCursor().getNearestMessage(METHOD_BLACKLIST);
-            assert blackList != null;
-            if (blackList.contains(expectedMethodName)) {
-                return method;
-            }
-            //WON'T DO: there is a rare edge case, that is not addressed yet.
-            // If `getFoo()` returns `ba` and `getBa()` returns `foo` then neither will be renamed.
-            // This could be fixed by compiling a list of planned changes and doing a soundness check (and not renaming sequentially, or rather introducing temporary method names)
-            // At this point I don't think it's worth the effort.
-
-
-            String pathToClass = method.getMethodType().getDeclaringType().getFullyQualifiedName().replace('$', '.');
-            //todo write separate recipe for merging effective setters
-            acc.renameRecords.add(
-                    new RenameRecord(
-                            pathToClass,
-                            actualMethodName,
-                            parameterType,
-                            expectedMethodName
-                    )
-            );
-            blackList.remove(actualMethodName);//actual method name becomes available again
-            blackList.add(expectedMethodName);//expected method name now blocked
-            return method;
-        }
+    @Override
+    public MethodAcc getInitialValue(ExecutionContext ctx) {
+        return new MethodAcc();
     }
 
-    private static JavaType. @Nullable Variable extractVariable(J.MethodDeclaration method) {
-        J.Assignment assignment_ = (J.Assignment) method.getBody().getStatements().get(0);
+    @Override
+    public TreeVisitor<?, ExecutionContext> getScanner(MethodAcc acc) {
+        return new JavaIsoVisitor<ExecutionContext>() {
 
-        JavaType.Variable fieldType;
-        if (assignment_.getVariable() instanceof J.FieldAccess) {
-            J.FieldAccess fieldAccess = (J.FieldAccess) assignment_.getVariable();
-            fieldType = fieldAccess.getName().getFieldType();
-        } else if (assignment_.getVariable() instanceof J.Identifier) {
-            J.Identifier fieldAccess = (J.Identifier) assignment_.getVariable();
-            fieldType = fieldAccess.getFieldType();
-        } else {
-            //only those types above are possible, see LombokUtils::isEffectivelySetter
-            throw new IllegalStateException("Unexpected type for returned variable");
-        }
-        return fieldType;
+            @Override
+            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+
+                List<String> doNotRename = classDecl.getBody().getStatements().stream()
+                        .filter(s -> s instanceof J.MethodDeclaration)
+                        .map(s -> (J.MethodDeclaration) s)
+                        .map(J.MethodDeclaration::getSimpleName)
+                        .collect(Collectors.toList());
+
+                getCursor().putMessage(DO_NOT_RENAME, doNotRename);
+
+                super.visitClassDeclaration(classDecl, ctx);
+
+                return classDecl;
+            }
+
+            @Override
+            public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
+                assert method.getMethodType() != null;
+
+                if (!LombokUtils.isEffectivelySetter(method)) {
+                    return method;
+                }
+
+                // return early if the method overrides another
+                // if the project defined both the original and the overridden method,
+                // then the renaming of the "original" in the base class will cover the override
+                if (method.getLeadingAnnotations().stream().anyMatch(a -> "Override".equals(a.getSimpleName()))) {
+                    return method;
+                }
+
+                JavaType.Variable fieldType = extractVariable(method);
+
+                String expectedMethodName = LombokUtils.deriveSetterMethodName(fieldType);
+                String parameterType = fieldType.getType().toString();
+                String actualMethodName = method.getSimpleName();
+
+                // If method already has the name it should have, then nothing to be done
+                if (expectedMethodName.equals(actualMethodName)) {
+                    return method;
+                }
+
+                // If the desired method name is already taken by an existing method, the current method cannot be renamed
+                List<String> doNotRename = getCursor().getNearestMessage(DO_NOT_RENAME);
+                assert doNotRename != null;
+                if (doNotRename.contains(expectedMethodName)) {
+                    return method;
+                }
+                //WON'T DO: there is a rare edge case, that is not addressed yet.
+                // If `getFoo()` returns `ba` and `getBa()` returns `foo` then neither will be renamed.
+                // This could be fixed by compiling a list of planned changes and doing a soundness check (and not renaming sequentially, or rather introducing temporary method names)
+                // At this point I don't think it's worth the effort.
+
+
+                String pathToClass = method.getMethodType().getDeclaringType().getFullyQualifiedName().replace('$', '.');
+                //todo write separate recipe for merging effective setters
+                acc.renameRecords.add(
+                        new RenameRecord(
+                                pathToClass,
+                                actualMethodName,
+                                parameterType,
+                                expectedMethodName
+                        )
+                );
+                doNotRename.remove(actualMethodName);//actual method name becomes available again
+                doNotRename.add(expectedMethodName);//expected method name now blocked
+                return method;
+            }
+
+            private JavaType.@Nullable Variable extractVariable(J.MethodDeclaration method) {
+                J.Assignment assignment_ = (J.Assignment) method.getBody().getStatements().get(0);
+
+                JavaType.Variable fieldType;
+                if (assignment_.getVariable() instanceof J.FieldAccess) {
+                    J.FieldAccess fieldAccess = (J.FieldAccess) assignment_.getVariable();
+                    fieldType = fieldAccess.getName().getFieldType();
+                } else if (assignment_.getVariable() instanceof J.Identifier) {
+                    J.Identifier fieldAccess = (J.Identifier) assignment_.getVariable();
+                    fieldType = fieldAccess.getFieldType();
+                } else {
+                    //only those types above are possible, see LombokUtils::isEffectivelySetter
+                    throw new IllegalStateException("Unexpected type for returned variable");
+                }
+                return fieldType;
+            }
+        };
     }
 
     @Override
