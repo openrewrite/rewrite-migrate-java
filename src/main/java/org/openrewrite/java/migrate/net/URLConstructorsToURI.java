7a21d21705086bacdb239365100098ca3dacc197
karthikNousher <emailkarthik369@gmail.com>
2025-02-26 14:20:45 +0000

    Improve Handling of Absolute, Relative, and Non-Literal URLs in URL Migration (#678)
    * added new recipe to add a new convert uri method.
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Ran formatter
    
    * Fix tests
    
    * Update logic
    
    * Update string literal check
    
    * Update list
    
    * add test
    
    * Fix test
    
    * Get constant value
    
    * Polish pre condition handling
    
    ---------
    
    Co-authored-by: KARTHIK NOUSHER <karthiknousher@ibm.com>
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Ralph-Sanders <ralph.sanders@ibm.com>
    Co-authored-by: Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>

--- src/main/java/org/openrewrite/java/migrate/net/URLConstructorsToURI.java
+++ src/main/java/org/openrewrite/java/migrate/net/URLConstructorsToURI.java
@@ -15,59 +15,129 @@
  */
 package org.openrewrite.java.migrate.net;
 
-import com.google.errorprone.refaster.annotation.AfterTemplate;
-import com.google.errorprone.refaster.annotation.BeforeTemplate;
-import org.openrewrite.java.template.RecipeDescriptor;
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.analysis.constantfold.ConstantFold;
+import org.openrewrite.analysis.util.CursorUtil;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.TypeUtils;
 
 import java.net.URI;
-import java.net.URL;
 
-public class URLConstructorsToURI {
-    @RecipeDescriptor(
-            name = "Convert `new URL(String)` to `URI.create(String).toURL()`",
-            description = "Converts `new URL(String)` constructors to `URI.create(String).toURL()`."
-    )
-    public static class URLSingleArgumentConstructor {
-        @BeforeTemplate
-        URL urlConstructor(String spec) throws Exception {
-            return new URL(spec);
-        }
+public class URLConstructorsToURI extends Recipe {
 
-        @AfterTemplate
-        URL uriCreateToURL(String spec) throws Exception {
-            return URI.create(spec).toURL();
-        }
+    private static final String URI_FQN = "java.net.URI";
+    private static final String URL_FQN = "java.net.URL";
+    private static final MethodMatcher methodMatcherSingleArg = new MethodMatcher(URL_FQN + " <constructor>(java.lang.String)");
+    private static final MethodMatcher methodMatcherThreeArg = new MethodMatcher(URL_FQN + " <constructor>(java.lang.String, java.lang.String, java.lang.String)");
+    private static final MethodMatcher methodMatcherFourArg = new MethodMatcher(URL_FQN + " <constructor>(java.lang.String, java.lang.String, int, java.lang.String)");
+
+    @Override
+    public String getDisplayName() {
+        return "Convert `new URL(String)` to `URI.create(String).toURL()`";
     }
 
-    @RecipeDescriptor(
-            name = "Convert `new URL(String, String, String)` to `new URI(...).toURL()`",
-            description = "Converts `new URL(String, String, String)` constructors to `new URI(...).toURL()`."
-    )
-    public static class URLThreeArgumentConstructor {
-        @BeforeTemplate
-        URL urlConstructor(String protocol, String host, String file) throws Exception {
-            return new URL(protocol, host, file);
-        }
-
-        @AfterTemplate
-        URL newUriToUrl(String protocol, String host, String file) throws Exception {
-            return new URI(protocol, null, host, -1, file, null, null).toURL();
-        }
+    @Override
+    public String getDescription() {
+        return "Converts `new URL(String)` constructors to `URI.create(String).toURL()`.";
     }
 
-    @RecipeDescriptor(
-            name = "Convert `new URL(String, String, int, String)` to `new URI(...).toURL()`",
-            description = "Converts `new URL(String, String, int, String)` constructors to `new URI(...).toURL()`."
-    )
-    public static class URLFourArgumentConstructor {
-        @BeforeTemplate
-        URL urlConstructor(String protocol, String host, int port, String file) throws Exception {
-            return new URL(protocol, host, port, file);
-        }
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new UsesType<>(URL_FQN, false),
+                new JavaVisitor<ExecutionContext>() {
+                    @Override
+                    public J visitNewClass(J.NewClass nc, ExecutionContext ctx) {
+                        if (methodMatcherSingleArg.matches(nc)) {
+                            String path = extractPath(nc.getArguments().get(0));
+                            if (isNotValidPath(path)) {
+                                return nc;
+                            }
 
-        @AfterTemplate
-        URL newUriToUrl(String protocol, String host, int port, String file) throws Exception {
-            return new URI(protocol, null, host, port, file, null, null).toURL();
-        }
+                            JavaTemplate template = JavaTemplate.builder("URI.create(#{any(String)}).toURL()")
+                                    .imports(URI_FQN)
+                                    .contextSensitive()
+                                    .javaParser(JavaParser.fromJavaVersion())
+                                    .build();
+                            maybeAddImport(URI_FQN);
+
+                            return template.apply(getCursor(),
+                                    nc.getCoordinates().replace(),
+                                    nc.getArguments().get(0));
+                        } else {
+                            if (methodMatcherThreeArg.matches(nc)) {
+                                JavaTemplate template = JavaTemplate.builder("new URI(#{any(String)}, null, #{any(String)}, -1, #{any(String)}, null, null).toURL()")
+                                        .imports(URI_FQN, URL_FQN)
+                                        .contextSensitive()
+                                        .javaParser(JavaParser.fromJavaVersion())
+                                        .build();
+
+                                maybeAddImport(URI_FQN);
+                                return template.apply(getCursor(), nc.getCoordinates().replace(),
+                                        nc.getArguments().get(0),
+                                        nc.getArguments().get(1),
+                                        nc.getArguments().get(2));
+                            } else if (methodMatcherFourArg.matches(nc)) {
+                                JavaTemplate template = JavaTemplate.builder("new URI(#{any(String)}, null, #{any(String)}, #{any(int)}, #{any(String)}, null, null).toURL()")
+                                        .imports(URI_FQN, URL_FQN)
+                                        .contextSensitive()
+                                        .javaParser(JavaParser.fromJavaVersion())
+                                        .build();
+
+                                maybeAddImport(URI_FQN);
+                                return template.apply(getCursor(), nc.getCoordinates().replace(),
+                                        nc.getArguments().get(0),
+                                        nc.getArguments().get(1),
+                                        nc.getArguments().get(2),
+                                        nc.getArguments().get(3));
+                            }
+                        }
+                        return super.visitNewClass(nc, ctx);
+                    }
+
+                    @Nullable
+                    private String extractPath(Expression arg) {
+                        if (arg instanceof J.Literal &&
+                                TypeUtils.isOfType(arg.getType(), JavaType.Primitive.String)) {
+                            // Check if value is not null
+                            String literalValueSource = ((J.Literal) arg).getValueSource();
+                            // Remove quotations from string
+                            return literalValueSource != null ? literalValueSource.substring(1, literalValueSource.length() - 1).trim() : null;
+                        } else if (arg instanceof J.Identifier &&
+                                TypeUtils.isOfType(arg.getType(), JavaType.Primitive.String)) {
+                            // find constant value of the identifier
+                            return CursorUtil.findCursorForTree(getCursor(), arg)
+                                    .bind(c -> ConstantFold.findConstantLiteralValue(c, String.class))
+                                    .toNull();
+                        } else {
+                            // null indicates no path extractable
+                            return null;
+                        }
+                    }
+
+                    private boolean isNotValidPath(@Nullable String path) {
+                        if (path == null) {
+                            return true;
+                        }
+
+                        try {
+                            //noinspection ResultOfMethodCallIgnored
+                            URI.create(path).toURL();
+                            return false;
+                        } catch (Exception e) {
+                            return true;
+                        }
+                    }
+                });
     }
 }
--- src/test/java/org/openrewrite/java/migrate/net/URLConstructorsToURITest.java
+++ src/test/java/org/openrewrite/java/migrate/net/URLConstructorsToURITest.java
@@ -26,7 +26,7 @@ import static org.openrewrite.java.Assertions.java;
 class URLConstructorsToURITest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.recipe(new URLConstructorsToURIRecipes());
+        spec.recipe(new URLConstructorsToURI());
     }
 
     @Test
@@ -53,9 +53,154 @@ class URLConstructorsToURITest implements RewriteTest {
 
               class Test {
                   void urlConstructor(String spec) throws Exception {
-                      URL url1 = URI.create(spec).toURL();
+                      URL url1 = new URL(spec);
                       URL url2 = new URI(spec, null, "localhost", -1, "file", null, null).toURL();
                       URL url3 = new URI(spec, null, "localhost", 8080, "file", null, null).toURL();
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-migrate-java/issues/620")
+    void urlCheckAbsolutePath() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.net.URL;
+
+              class Test {
+                  void urlConstructor() {
+                      URL url1 = new URL("https://test.com");
+                  }
+              }
+              """,
+            """
+              import java.net.URI;
+              import java.net.URL;
+
+              class Test {
+                  void urlConstructor() {
+                      URL url1 = URI.create("https://test.com").toURL();
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-migrate-java/issues/620")
+    void urlCheckConstantAbsolutePath() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.net.URL;
+
+              class Test {
+                  private String goodURL = "https://test.com";
+                  private String badURL = "not/valid/url";
+                  void urlConstructor() {
+                      URL url1 = new URL(goodURL);
+                  }
+              }
+              """,
+            """
+              import java.net.URI;
+              import java.net.URL;
+
+              class Test {
+                  private String goodURL = "https://test.com";
+                  private String badURL = "not/valid/url";
+                  void urlConstructor() {
+                      URL url1 = URI.create(goodURL).toURL();
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-migrate-java/issues/620")
+    void urlCheckConstantRelativePath() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.net.URL;
+
+              class Test {
+                  private String goodURL = "https://test.com";
+                  private String badURL = "not/valid/url";
+                  void urlConstructor() {
+                      URL url1 = new URL(badURL);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-migrate-java/issues/620")
+    void urlCheckRelativePath() {
+        rewriteRun(
+          spec -> spec.expectedCyclesThatMakeChanges(0),
+          //language=java
+          java(
+            """
+              import java.net.URL;
+
+              class Test {
+                  void urlConstructor() {
+                      URL url1 = new URL("TEST-INF/test/testCase.wsdl");
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-migrate-java/issues/620")
+    void urlCheckNullPath() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.net.URL;
+
+              class Test {
+                  void urlConstructor() {
+                      URL url1 = new URL(null);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-migrate-java/issues/620")
+    void urlCheckMethodInvocationParameter() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.net.URL;
+
+              class Test {
+                  void urlConstructor(String spec) throws Exception {
+                      URL url1 = new URL(getString());
+                  }
+
+                  String getString() {
+                      return "myURL";
                   }
               }
               """
