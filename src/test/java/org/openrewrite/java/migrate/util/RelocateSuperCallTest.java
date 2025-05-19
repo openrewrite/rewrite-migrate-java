9b9aed64bc854c373c05e7a7332591a37872c9d2
Tim te Beek <timtebeek@gmail.com>
2025-05-20 00:00:02 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/migrate/util/RelocateSuperCallTest.java
+++ src/test/java/org/openrewrite/java/migrate/util/RelocateSuperCallTest.java
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.migrate.util;
+
+import org.openrewrite.DocumentExample;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
@@ -15,6 +32,8 @@ class RelocateSuperCallTest implements RewriteTest {
           .allSources(src -> src.markers(javaVersion(25)));
 
     }
+
+    @DocumentExample
     @Test
     void relocateSuperAfterIf() {
         rewriteRun(
