d69ad7dc1faaf974861bbb223bd06b42bdd17d59
rkampani <rkampani@gmail.com>
2025-05-19 20:46:47 -0500

    Moved the file out of the util package and updated the JDK 25 rewrite YAML configuration to reflect the change

--- src/test/java/org/openrewrite/java/migrate/RelocateSuperCallTest.java
+++ src/test/java/org/openrewrite/java/migrate/RelocateSuperCallTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.migrate.util;
+package org.openrewrite.java.migrate;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
