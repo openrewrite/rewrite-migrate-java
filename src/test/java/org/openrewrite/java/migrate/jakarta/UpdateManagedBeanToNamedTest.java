6ed76d18af9394e9c040e56e471b06ddaf992bc7
Evie Lau <689163+evie-lau@users.noreply.github.com>
2025-06-03 16:11:45 -0400

    Add recipe to replace ManagedBean with Named annotation (#753)
    * Add recipe and test for replacing ManagedBean with Named annotation
    
    * Update description with EE levels
    
    * Copyright and formatting
    
    * Apply suggestions from code review
    
    Review suggestions
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    
    * Add jakarta inject jar, use precondition
    
    * Simplify getting annotation name value
    
    * Use a single annotation matcher with a wildcard
    
    * Update faces recipe lists
    
    * Add tags to recipe
    
    * Cleanup
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/test/java/org/openrewrite/java/migrate/jakarta/UpdateManagedBeanToNamedTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.jakarta;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class UpdateManagedBeanToNamedTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "jsf-api-2.1.29-11", "jakarta.faces-api-3.0.0"))
+          .recipe(new UpdateManagedBeanToNamed());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"javax", "jakarta"})
+    void updateManagedBeanToNamed(String pkg) {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import %s.faces.bean.ManagedBean;
+
+              @ManagedBean
+              public class ApplicationBean2 {
+              }
+              """.formatted(pkg),
+            """
+              import jakarta.inject.Named;
+
+              @Named
+              public class ApplicationBean2 {
+              }
+              """
+          )
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"javax", "jakarta"})
+    void updateManagedBeanToNamedWithArg(String pkg) {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import %s.faces.bean.ManagedBean;
+
+              @ManagedBean(name="myBean")
+              public class ApplicationBean2 {
+              }
+              """.formatted(pkg),
+            """
+              import jakarta.inject.Named;
+
+              @Named("myBean")
+              public class ApplicationBean2 {
+              }
+              """
+          )
+        );
+    }
+}
