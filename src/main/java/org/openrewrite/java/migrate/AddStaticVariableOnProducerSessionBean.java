2ece6d3fb3ec4bc836f658987b3f4591e2aa6238
Tim te Beek <timtebeek@gmail.com>
2025-06-05 15:23:54 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/java/migrate/AddStaticVariableOnProducerSessionBean.java
+++ src/main/java/org/openrewrite/java/migrate/AddStaticVariableOnProducerSessionBean.java
@@ -1,4 +1,18 @@
-package org.openrewrite.java.migrate;
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import lombok.val;
 import org.openrewrite.ExecutionContext;
@@ -24,6 +38,7 @@ public class AddStaticVariableOnProducerSessionBean extends Recipe {
     public TreeVisitor<?, ExecutionContext> getVisitor() {
         return new JavaVisitor<ExecutionContext>() {
 
+            @Override
             public J visitClassDeclaration(J.ClassDeclaration cs, ExecutionContext ctx) {
                 boolean isSessionBean = cs.getLeadingAnnotations().stream()
                         .map(J.Annotation::getSimpleName)
