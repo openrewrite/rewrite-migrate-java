00b8a106dc14601fa4b3950b6bc1a7e5da5a5fb2
Evie Lau <689163+evie-lau@users.noreply.github.com>
2025-06-05 17:14:36 -0500

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/java/migrate/AddStaticVariableOnProducerSessionBean.java
+++ src/main/java/org/openrewrite/java/migrate/AddStaticVariableOnProducerSessionBean.java
@@ -1,4 +1,18 @@
-package org.openrewrite.java.migrate;
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import org.openrewrite.*;
 import org.openrewrite.java.JavaVisitor;
@@ -28,7 +42,7 @@ public class AddStaticVariableOnProducerSessionBean extends Recipe {
         return Preconditions.check(new UsesType<>("jakarta.enterprise.inject.Produces", false),
                 new JavaVisitor<ExecutionContext>() {
                     @Override
-                    public J visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext executionContext) {
+                    public J visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext ctx) {
 
                         J.ClassDeclaration visitingClass = getCursor().firstEnclosing(J.ClassDeclaration.class);
                         boolean isSessionBean = visitingClass.getLeadingAnnotations().stream()
@@ -48,7 +62,7 @@ public class AddStaticVariableOnProducerSessionBean extends Recipe {
                                 return multiVariable.withModifiers(updatedModifiers);
                             }
                         }
-                        return super.visitVariableDeclarations(multiVariable, executionContext);
+                        return super.visitVariableDeclarations(multiVariable, ctx);
                     }
                 });
     }
