621dd207ba670c71b63fb362d2e00f0aba531598
lingenj <jacob.van.lingen@moderne.io>
2025-06-13 13:04:17 +0200

    Add only declarable Gradle jax-ws runtime dependencies

--- src/main/java/org/openrewrite/java/migrate/javax/AddJaxwsRuntime.java
+++ src/main/java/org/openrewrite/java/migrate/javax/AddJaxwsRuntime.java
@@ -116,12 +116,10 @@ public class AddJaxwsRuntime extends Recipe {
                         Set<String> runtimeConfigurations = getTransitiveDependencyConfiguration(gp, SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT);
                         if (runtimeConfigurations.isEmpty()) {
                             if (gp.getConfiguration("compileOnly") != null) {
-                                g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, "compileOnly", null, null, null, null, null)
-                                        .visitNonNull(g, ctx);
+                                g = addJaxWsRuntimeDependency("compileOnly", g, ctx);
                             }
                             if (gp.getConfiguration("testImplementation") != null) {
-                                g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, "testImplementation", null, null, null, null, null)
-                                        .visitNonNull(g, ctx);
+                                g = addJaxWsRuntimeDependency("testImplementation", g, ctx);
                             }
                         } else {
                             for (String apiConfiguration : apiConfigurations) {
@@ -131,8 +129,7 @@ public class AddJaxwsRuntime extends Recipe {
                                     GradleDependencyConfiguration runtimeGdc = gp.getConfiguration(runtimeConfiguration);
                                     List<GradleDependencyConfiguration> runtimeTransitives = gp.configurationsExtendingFrom(runtimeGdc, true);
                                     if (apiTransitives.stream().noneMatch(runtimeTransitives::contains)) {
-                                        g = (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, apiConfiguration, null, null, null, null, null)
-                                                .visitNonNull(g, ctx);
+                                        g = addJaxWsRuntimeDependency(apiConfiguration, g, ctx);
                                     }
                                 }
                             }
@@ -169,6 +166,14 @@ public class AddJaxwsRuntime extends Recipe {
                     }
 
                     return configurations;
+                }
+
+                private G.CompilationUnit addJaxWsRuntimeDependency(String apiConfiguration, G.CompilationUnit g, ExecutionContext ctx) {
+                    if (apiConfiguration.endsWith("Classpath")) {
+                        return g;
+                    }
+                    return (G.CompilationUnit) new org.openrewrite.gradle.AddDependencyVisitor(SUN_JAXWS_RUNTIME_GROUP, SUN_JAXWS_RUNTIME_ARTIFACT, "2.3.x", null, apiConfiguration, null, null, null, null, null)
+                            .visitNonNull(g, ctx);
                 }
             });
         }
