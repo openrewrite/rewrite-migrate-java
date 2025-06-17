bdb3b46caa76298df48de5f36a0e210e55d6274e
Jente Sondervorst <jentesondervorst@gmail.com>
2025-06-17 17:19:04 +0200

    Fix tests + licenses

--- src/test/java/org/openrewrite/java/migrate/lang/PatternMatchingWithInstanceOfTest.java
+++ src/test/java/org/openrewrite/java/migrate/lang/PatternMatchingWithInstanceOfTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.migrate.lang;
 
 import org.junit.jupiter.api.Test;
@@ -84,8 +99,6 @@ class PatternMatchingWithInstanceOfTest implements RewriteTest {
               """,
             """
             class Test {
-                static class Cat {}
-                static class Dog {}
                 void test(Object o) {
                     if (o instanceof Integer number) {
                         System.out.println(number);
@@ -168,16 +181,7 @@ class PatternMatchingWithInstanceOfTest implements RewriteTest {
                       }
                   }
               }
-              """,
-            """
-            class Test {
-                void test(Object o) {
-                    if (o instanceof Boolean) {
-                        System.out.println("we ignore the unused values");
-                    }
-                }
-            }
-            """
+              """
           )
         );
     }
@@ -258,19 +262,7 @@ class PatternMatchingWithInstanceOfTest implements RewriteTest {
                       }
                   }
               }
-              """,
-            """
-            class Test {
-                static class Dog {}
-                void test(Object o) {
-                    if (o instanceof Dog) {
-                        Dog dog = new Dog();
-                        Dog oAsDog = new Dog();
-                        System.out.println("My pet is a" + (Dog) o);
-                    }
-                }
-            }
-            """
+              """
           )
         );
     }
