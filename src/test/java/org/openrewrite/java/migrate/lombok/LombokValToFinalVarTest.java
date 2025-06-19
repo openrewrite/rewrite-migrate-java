8ab619fd9af38f627077efeb51081c731765a925
Tim te Beek <tim@moderne.io>
2025-06-19 08:25:09 +0200

    Apply suggestions from code review

--- src/test/java/org/openrewrite/java/migrate/lombok/LombokValToFinalVarTest.java
+++ src/test/java/org/openrewrite/java/migrate/lombok/LombokValToFinalVarTest.java
@@ -18,7 +18,6 @@ package org.openrewrite.java.migrate.lombok;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
-import org.openrewrite.Issue;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
