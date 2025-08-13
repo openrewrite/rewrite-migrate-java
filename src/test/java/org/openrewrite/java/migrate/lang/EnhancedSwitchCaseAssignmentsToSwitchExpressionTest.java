628f5fc44701565087b041677c47ca14b8d0fe88
Jacob van Lingen <jacob.van.lingen@moderne.io>
2025-08-13 09:47:38 +0200

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/migrate/lang/EnhancedSwitchCaseAssignmentsToSwitchExpressionTest.java
+++ src/test/java/org/openrewrite/java/migrate/lang/EnhancedSwitchCaseAssignmentsToSwitchExpressionTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.java.migrate.lang;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
