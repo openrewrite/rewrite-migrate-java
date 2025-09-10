e5b47da8316c38f43a1f98d2f7b676ba5364f3fb
Johan Kragt <johan.kragt@moderne.io>
2025-09-10 14:06:59 +0200

    polish

--- src/main/java/org/openrewrite/java/migrate/util/MigrateStringReaderToReaderOf.java
+++ src/main/java/org/openrewrite/java/migrate/util/MigrateStringReaderToReaderOf.java
@@ -36,6 +36,7 @@ import org.openrewrite.java.tree.TypeUtils;
 @Value
 public class MigrateStringReaderToReaderOf extends Recipe {
     private static final MethodMatcher STRING_READER_CONSTRUCTOR = new MethodMatcher("java.io.StringReader <constructor>(java.lang.String)");
+    private static final MethodMatcher TO_STRING_METHOD = new MethodMatcher("java.lang.Object toString()", true);
 
     @Override
     public String getDisplayName() {
@@ -59,42 +60,42 @@ public class MigrateStringReaderToReaderOf extends Recipe {
                 new JavaVisitor<ExecutionContext>() {
 
                     @Override
-                    public J visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext ctx) {
-                        if (TypeUtils.isOfClassType(multiVariable.getTypeAsFullyQualified(), "java.io.Reader")) {
-                            return multiVariable.withVariables(ListUtils.map(multiVariable.getVariables(), v -> {
+                    public J visitVariableDeclarations(J.VariableDeclarations mV, ExecutionContext ctx) {
+                        if (TypeUtils.isOfClassType(mV.getTypeAsFullyQualified(), "java.io.Reader")) {
+                            return mV.withVariables(ListUtils.map(mV.getVariables(), v -> {
                                 maybeRemoveImport("java.io.StringReader");
                                 maybeAddImport("java.io.Reader");
-                                return (J.VariableDeclarations.NamedVariable) new TransformVisitor().visit(v, ctx, getCursor().getParent());
+                                return (J.VariableDeclarations.NamedVariable) new TransformVisitor().visitNonNull(v, ctx, getCursor().getParent());
                             }));
                         }
-                        return super.visitVariableDeclarations(multiVariable, ctx);
+                        return super.visitVariableDeclarations(mV, ctx);
                     }
 
                     @Override
-                    public J visitAssignment(J.Assignment assignment, ExecutionContext ctx) {
-                        if (assignment.getVariable() instanceof J.Identifier) {
-                            J.Identifier variable = (J.Identifier) assignment.getVariable();
+                    public J visitAssignment(J.Assignment a, ExecutionContext ctx) {
+                        if (a.getVariable() instanceof J.Identifier) {
+                            J.Identifier variable = (J.Identifier) a.getVariable();
                             if (TypeUtils.isOfClassType(variable.getType(), "java.io.Reader")) {
                                 maybeRemoveImport("java.io.StringReader");
                                 maybeAddImport("java.io.Reader");
-                                return new TransformVisitor().visit(assignment, ctx, getCursor().getParent());
+                                return new TransformVisitor().visitNonNull(a, ctx, getCursor().getParent());
                             }
                         }
-                        return super.visitAssignment(assignment, ctx);
+                        return super.visitAssignment(a, ctx);
                     }
 
                     @Override
-                    public J visitReturn(J.Return return_, ExecutionContext ctx) {
+                    public J visitReturn(J.Return r, ExecutionContext ctx) {
                         J.MethodDeclaration method = getCursor().firstEnclosing(J.MethodDeclaration.class);
                         if (method != null && method.getReturnTypeExpression() != null) {
                             JavaType returnType = method.getReturnTypeExpression().getType();
                             if (TypeUtils.isOfClassType(returnType, "java.io.Reader")) {
                                 maybeRemoveImport("java.io.StringReader");
                                 maybeAddImport("java.io.Reader");
-                                return new TransformVisitor().visit(return_, ctx, getCursor().getParent());
+                                return new TransformVisitor().visitNonNull(r, ctx, getCursor().getParent());
                             }
                         }
-                        return super.visitReturn(return_, ctx);
+                        return super.visitReturn(r, ctx);
                     }
 
                     private class TransformVisitor extends JavaVisitor<ExecutionContext> {
@@ -105,7 +106,6 @@ public class MigrateStringReaderToReaderOf extends Recipe {
                                 argument = optimizeCharSequenceToString(argument);
                                 JavaTemplate template = JavaTemplate.builder("Reader.of(#{any(java.lang.CharSequence)})")
                                         .imports("java.io.Reader")
-                                        .contextSensitive()
                                         .build();
 
                                 return template.apply(getCursor(), newClass.getCoordinates().replace(), argument);
@@ -116,8 +116,7 @@ public class MigrateStringReaderToReaderOf extends Recipe {
                         private Expression optimizeCharSequenceToString(Expression expr) {
                             if (expr instanceof J.MethodInvocation) {
                                 J.MethodInvocation mi = (J.MethodInvocation) expr;
-                                if ("toString".equals(mi.getSimpleName()) &&
-                                        (mi.getArguments().isEmpty() || (mi.getArguments().size() == 1 && mi.getArguments().get(0) instanceof J.Empty)) &&
+                                if (TO_STRING_METHOD.matches(mi) &&
                                         mi.getSelect() != null && TypeUtils.isAssignableTo("java.lang.CharSequence", mi.getSelect().getType())) {
                                     return mi.getSelect();
                                 }
