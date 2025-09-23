6e9bae9e27784f7aa2dd8d630810f85d80c19156
Jente Sondervorst <jentesondervorst@gmail.com>
2025-09-23 13:51:58 +0200

    Fixed the whiteline removal for the schemaLocations when already properly migrated

--- src/test/java/org/openrewrite/java/migrate/jakarta/JavaxWebXmlToJakartaWebXmlTest.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/JavaxWebXmlToJakartaWebXmlTest.java
@@ -172,7 +172,7 @@ class JavaxWebXmlToJakartaWebXmlTest implements RewriteTest {
                   <web-app xmlns="https://jakarta.ee/xml/ns/jakartaee"
                            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                            xsi:schemaLocation="https://jakarta.ee/xml/ns/jakartaee
-                            https://jakarta.ee/xml/ns/jakartaee/web-app_5_3.xsd"
+                            https://jakarta.ee/xml/ns/jakartaee/web-app_5_0.xsd"
                            version="5.0">
                       <display-name>Unit testing</display-name>
                   </web-fragment>
