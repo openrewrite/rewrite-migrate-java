4cd03e21d3bcfd58e238c4acb5a54b1bc60625bb
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-10-31 11:43:25 +0100

    re add import

--- src/test/java/org/openrewrite/java/migrate/maven/MigrateJaxwsMavenPluginTest.java
+++ src/test/java/org/openrewrite/java/migrate/maven/MigrateJaxwsMavenPluginTest.java
@@ -17,6 +17,7 @@ package org.openrewrite.java.migrate.maven;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.maven.Assertions.pomXml;
