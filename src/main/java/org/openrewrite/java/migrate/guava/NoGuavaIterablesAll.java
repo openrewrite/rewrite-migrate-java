0e683426cd71ffc4265214ef7fd435f45b18f678
Tim te Beek <tim@moderne.io>
2025-11-16 21:27:55 +0100

    Convert `Iterables.all(Collection, Predictate)` into `.stream().allMatch(Predicate)`
    Fixes #920

---
+++ src/main/java/org/openrewrite/java/migrate/guava/NoGuavaIterablesAll.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.guava;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.util.Set;
+
+import static java.util.Collections.singleton;
+
+public class NoGuavaIterablesAll extends Recipe {
+    private static final MethodMatcher ITERABLES_ALL = new MethodMatcher("com.google.common.collect.Iterables all(java.lang.Iterable, com.google.common.base.Predicate)");
+
+    @Override
+    public String getDisplayName() {
+        return "Prefer `Collection.stream().allMatch(Predicate)`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Prefer `Collection.stream().allMatch(Predicate)` over `Iterables.all(Collection, Predicate)`.";
+    }
+
+    @Override
+    public Set<String> getTags() {
+        return singleton("guava");
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new UsesMethod<>(ITERABLES_ALL), new JavaVisitor<ExecutionContext>() {
+            @Override
+            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                if (ITERABLES_ALL.matches(method)) {
+                    maybeRemoveImport("com.google.common.base.Predicate");
+                    maybeRemoveImport("com.google.common.collect.Iterables");
+                    maybeAddImport("java.util.function.Predicate");
+
+                    if (TypeUtils.isAssignableTo("java.util.Collection", method.getArguments().get(0).getType())) {
+                        return JavaTemplate.builder("#{any(java.util.Collection)}.stream().allMatch(#{any(java.util.function.Predicate)})")
+                                .build()
+                                .apply(getCursor(),
+                                        method.getCoordinates().replace(),
+                                        method.getArguments().get(0),
+                                        method.getArguments().get(1));
+                    }
+                    return method;
+                }
+                return super.visitMethodInvocation(method, ctx);
+            }
+        });
+    }
+}
