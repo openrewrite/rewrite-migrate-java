96d6dd97088cf3edade46fee2ec4f42fc7c6b10a
Markus KARG <markus@headcrashing.eu>
2026-04-07 23:51:18 +0200

    Replace Collections.emptyXXX with Immutable Static Factory Methods (#1045)
    * Replace Collections.emptyXXX with Immutable Static Factory Methods
    
    Closes #1044
    
    * Add Java 9+ precondition and use JavaTemplate for Collections.emptyXXX recipes
    
    * Add all util recipes to JavaUtilAPIs and reference it from best practices
    
    * Fix ReplaceMathRandomWithThreadLocalRandom recipe class name
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/main/java/org/openrewrite/java/migrate/util/MigrateCollectionsEmptyMap.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.util;
+
+import lombok.Getter;
+import org.openrewrite.*;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesJavaVersion;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.J;
+
+public class MigrateCollectionsEmptyMap extends Recipe {
+    private static final MethodMatcher EMPTY_MAP = new MethodMatcher("java.util.Collections emptyMap()");
+
+    @Getter
+    final String displayName = "Prefer `Map.of()`";
+
+    @Getter
+    final String description = "Prefer `Map.of()` instead of using `Collections.emptyMap()` in Java 9 or higher.";
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        TreeVisitor<?, ExecutionContext> check = Preconditions.and(new UsesJavaVersion<>(9),
+                new UsesMethod<>(EMPTY_MAP));
+        return Preconditions.check(check, new JavaIsoVisitor<ExecutionContext>() {
+
+            @Override
+            public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                J.MethodInvocation m = super.visitMethodInvocation(method, ctx);
+
+                if (EMPTY_MAP.matches(m)) {
+                    maybeRemoveImport("java.util.Collections");
+                    maybeAddImport("java.util.Map");
+                    return JavaTemplate.builder("Map.of()")
+                            .contextSensitive()
+                            .imports("java.util.Map")
+                            .build()
+                            .apply(updateCursor(m), m.getCoordinates().replace());
+                }
+
+                return m;
+            }
+        });
+    }
+}
---
+++ src/main/java/org/openrewrite/java/migrate/util/MigrateCollectionsEmptySet.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.migrate.util;
+
+import lombok.Getter;
+import org.openrewrite.*;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesJavaVersion;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.J;
+
+public class MigrateCollectionsEmptySet extends Recipe {
+    private static final MethodMatcher EMPTY_SET = new MethodMatcher("java.util.Collections emptySet()");
+
+    @Getter
+    final String displayName = "Prefer `Set.of()`";
+
+    @Getter
+    final String description = "Prefer `Set.of()` instead of using `Collections.emptySet()` in Java 9 or higher.";
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        TreeVisitor<?, ExecutionContext> check = Preconditions.and(new UsesJavaVersion<>(9),
+                new UsesMethod<>(EMPTY_SET));
+        return Preconditions.check(check, new JavaIsoVisitor<ExecutionContext>() {
+
+            @Override
+            public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                J.MethodInvocation m = super.visitMethodInvocation(method, ctx);
+
+                if (EMPTY_SET.matches(m)) {
+                    maybeRemoveImport("java.util.Collections");
+                    maybeAddImport("java.util.Set");
+                    return JavaTemplate.builder("Set.of()")
+                            .contextSensitive()
+                            .imports("java.util.Set")
+                            .build()
+                            .apply(updateCursor(m), m.getCoordinates().replace());
+                }
+
+                return m;
+            }
+        });
+    }
+}
