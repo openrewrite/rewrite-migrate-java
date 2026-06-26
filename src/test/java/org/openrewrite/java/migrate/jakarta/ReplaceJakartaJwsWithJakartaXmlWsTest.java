c163f8e3531046836b577f8db696dd0ba9c11398
Tim te Beek <tim@moderne.io>
2026-06-26 10:42:13 +0200

    Use ChangeDependency for in-place jws-api replacement; assert exact replacement in test

--- src/test/java/org/openrewrite/java/migrate/jakarta/ReplaceJakartaJwsWithJakartaXmlWsTest.java
+++ src/test/java/org/openrewrite/java/migrate/jakarta/ReplaceJakartaJwsWithJakartaXmlWsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 the original author or authors.
+ * Copyright 2026 the original author or authors.
  * <p>
  * Licensed under the Moderne Source Available License (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.openrewrite.maven.Assertions.pomXml;
 
 class ReplaceJakartaJwsWithJakartaXmlWsTest implements RewriteTest {
@@ -32,7 +31,7 @@ class ReplaceJakartaJwsWithJakartaXmlWsTest implements RewriteTest {
 
     @DocumentExample
     @Test
-    void removesJakartaJwsApiAndAddsJakartaXmlWsApi() {
+    void replacesJakartaJwsApiInPlaceWithJakartaXmlWsApi() {
         rewriteRun(
           //language=xml
           pomXml(
@@ -48,18 +47,24 @@ class ReplaceJakartaJwsWithJakartaXmlWsTest implements RewriteTest {
                           <artifactId>jakarta.jws-api</artifactId>
                           <version>3.0.0</version>
                       </dependency>
-                      <dependency>
-                          <groupId>jakarta.xml.ws</groupId>
-                          <artifactId>jakarta.xml.ws-api</artifactId>
-                          <version>3.0.0</version>
-                      </dependency>
                   </dependencies>
               </project>
               """,
-            spec -> spec.after(pom -> assertThat(pom)
-              .doesNotContain("jakarta.jws")
-              .contains("jakarta.xml.ws")
-              .actual())
+            """
+              <project>
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>com.example</groupId>
+                  <artifactId>demo</artifactId>
+                  <version>0.0.1-SNAPSHOT</version>
+                  <dependencies>
+                      <dependency>
+                          <groupId>jakarta.xml.ws</groupId>
+                          <artifactId>jakarta.xml.ws-api</artifactId>
+                          <version>4.0.3</version>
+                      </dependency>
+                  </dependencies>
+              </project>
+              """
           )
         );
     }
