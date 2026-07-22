cddded884eb9f0e1d7833589809eb76c726a4b16
tim@moderne.io <tim@moderne.io>
2026-07-22 12:12:52 +0000

    Move Develocity to community.develocity.cloud
    Co-authored-by: Moderne <team@moderne.io>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 develocity {
-    server = "https://ge.openrewrite.org/"
+    server = "https://community.develocity.cloud/"
     val isCiServer = System.getenv("CI")?.equals("true") ?: false
     val accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
     val authenticated = !accessKey.isNullOrBlank()
