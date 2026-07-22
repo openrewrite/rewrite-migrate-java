ab370a9db1a8b641a5d62a1187c980612d971364
Tim te Beek <tim@moderne.io>
2026-07-22 17:03:11 +0200

    Set Develocity `projectId` to "openrewrite" (#1172)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ plugins {
 
 develocity {
     server = "https://community.develocity.cloud/"
+    projectId = "openrewrite"
     val isCiServer = System.getenv("CI")?.equals("true") ?: false
     val accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
     val authenticated = !accessKey.isNullOrBlank()
